cc89c097e50441da82b3e8571fd5a3bd5fdbbbb1
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2023-09-07 05:46:07 +0200

    Move dependencies to webpack and compile bundle (#14)

---
+++ app/templates/src/js/main.js
@@ -0,0 +1,7 @@
+/* globals Chart:false, feather:false */
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
---
+++ app/templates/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, '../static/js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: () => [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}
