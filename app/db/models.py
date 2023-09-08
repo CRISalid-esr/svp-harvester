c61bdc50472934ec0dfa379ce2ab85a6f69ebf32
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-09-08 15:49:33 +0200

    Add entities, people, identifiers to the model

--- app/db/models.py
+++ app/db/models.py
@@ -14,6 +14,9 @@ class Retrieval(Base):
         back_populates="retrieval", cascade="all, delete"
     )
 
+    entity_id: Mapped[int] = mapped_column(ForeignKey("entities.id"))
+    entity: Mapped["Entity"] = relationship(back_populates="retrievals")
+
 
 class Harvesting(Base):
     __tablename__ = "harvestings"
@@ -22,3 +25,46 @@ class Harvesting(Base):
     harvester = Column(String, index=True)
     retrieval_id: Mapped[int] = mapped_column(ForeignKey("retrievals.id"))
     retrieval: Mapped["Retrieval"] = relationship(back_populates="harvesters")
+
+
+class Entity(Base):
+    __tablename__ = "entities"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str]
+
+    __mapper_args__ = {
+        "polymorphic_identity": "employee",
+        "polymorphic_on": "type",
+    }
+
+    identifiers: Mapped[List["Identifier"]] = relationship(
+        back_populates="entity", cascade="all, delete"
+    )
+
+    retrievals: Mapped[List["Retrieval"]] = relationship(
+        back_populates="entity", cascade="all, delete"
+    )
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name!r})"
+
+
+class Identifier(Base):
+    __tablename__ = "identifiers"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    type = Column(String, index=True)
+    entity_id: Mapped[int] = mapped_column(ForeignKey("entities.id"))
+    entity: Mapped["Entity"] = relationship(back_populates="identifiers")
+
+
+class Person(Entity):
+    __tablename__ = "people"
+    id: Mapped[int] = mapped_column(ForeignKey("entities.id"), primary_key=True)
+    last_name: Mapped[str]
+    first_name: Mapped[str]
+
+    __mapper_args__ = {
+        "polymorphic_identity": "person",
+    }
