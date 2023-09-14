b6f7c4f181c5929c6a8a06ff87639b699427db20
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-09-14 17:51:32 +0200

    Move new middleware declaration to SVPHarvester Class

--- app/main.py
+++ app/main.py
@@ -2,12 +2,9 @@
 
 import uvicorn
 
-from app.logging.logging_middleware import log_middleware
 from app.svp_harvester import SvpHarvester
 
 app = SvpHarvester()
-
-app.middleware("http")(log_middleware)
 
 if __name__ == "__main__":  # pragma: no cover
     uvicorn.run(app, host="0.0.0.0", port=8000)
