452fb9b44b622c711f62992dfa0fa93c6aa37e13
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-09-22 16:03:48 +0200

    Adapt tests

--- tests/test_harvesters/test_hal/test_hal_api_query_builder.py
+++ tests/test_harvesters/test_hal/test_hal_api_query_builder.py
@@ -21,9 +21,11 @@ def fixture_hal_api_query_builder():
 
 def test_set_query(hal_query_builder):
     """Test if the set_query function return the values set"""
-    hal_query_builder.set_query("test_type", "test_value")
+    hal_query_builder.set_query(
+        hal_query_builder.QueryParameters.AUTH_ID_HAL_I, "test_value"
+    )
 
-    assert hal_query_builder.identifier_type == "test_type"
+    assert hal_query_builder.identifier_type.value == "authIdHal_i"
     assert hal_query_builder.identifier_value == "test_value"
 
 
@@ -47,26 +49,15 @@ def test_build_query(hal_query_builder):
     assert result_dict == expected_result
 
 
-def test_build_query_without_any_identifier_set(hal_query_builder):
-    """Test if the build function raise an error if the parameters are not defined"""
-    hal_query_builder.set_query(None, None)
-
-    with pytest.raises(AssertionError):
-        hal_query_builder.build()
-
-
-def test_build_query_without_identifier_type_set(hal_query_builder):
-    """Test if the build function raise an error if the parameters are not defined"""
-    hal_query_builder.set_query(None, "test_value")
-
-    with pytest.raises(AssertionError):
-        hal_query_builder.build()
+def test_build_query_with_invalid_identifier_type(hal_query_builder):
+    """Test if the set_query function raise an error if the identifier type is not valid"""
+    with pytest.raises(TypeError):
+        hal_query_builder.set_query("foobar", "test_value")
 
 
 def test_build_query__without_identifier_value_set(hal_query_builder):
-    """Test if the build function raise an error if the parameters are not defined"""
-
-    hal_query_builder.set_query("test_type", None)
+    """Test if the build function raise an error if the value of the identifier is not set"""
+    hal_query_builder.set_query(hal_query_builder.QueryParameters.AUTH_ID_HAL_I, None)
 
     with pytest.raises(AssertionError):
         hal_query_builder.build()
@@ -74,6 +65,5 @@ def test_build_query__without_identifier_value_set(hal_query_builder):
 
 def test_build_query__without_set_query_set(hal_query_builder):
     """Test if the build function raise an error if the set_query is not set"""
-
     with pytest.raises(AssertionError):
         hal_query_builder.build()
