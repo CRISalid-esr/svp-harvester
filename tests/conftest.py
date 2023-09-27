c506f442f1e94976ced50e6d6660de472f408a95
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2023-09-27 13:22:23 +0200

    Hotfix : duplicated concept bug (#69)
    Prevent sqlalchemy failure when a publication comes with a concept that is duplicated and that already exists in database

--- tests/conftest.py
+++ tests/conftest.py
@@ -282,6 +282,16 @@ def fixture_hal_api_docs_for_one_researcher(_base_path):
     return _hal_api_json_data_from_file(_base_path, "docs_for_one_researcher")
 
 
+@pytest.fixture(name="hal_api_docs_with_same_kw_twice")
+def fixture_hal_api_docs_with_same_kw_twice(_base_path):
+    """
+    Generate a HAL API response for one researcher in JSON format
+    :param _base_path: test data directory base
+    :return: HAL API response for one researcher in JSON format
+    """
+    return _hal_api_json_data_from_file(_base_path, "docs_with_same_kw_twice")
+
+
 def _hal_api_json_data_from_file(base_path, file_name):
     file_path = f"data/hal_api/{file_name}.json"
     return _json_data_from_file(base_path, file_path)
