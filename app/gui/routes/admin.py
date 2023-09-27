a2575a7b6d5aa71b3be9bc1ab7e5ee6e3b4d78e7
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2023-09-27 06:57:58 +0200

    Hotfix disable middlewares (#62)
    * Disable all middlewares & Update application
    
    All middlewares have been disabled due to Faspi/Starlette bug
    https://stackoverflow.com/questions/70043665/fastapi-unvicorn-request-hanging-during-invocation-of-call-next-path-operation
    https://stackoverflow.com/questions/68830274/blocked-code-while-using-middleware-and-dependency-injections-to-log-requests-in

--- app/gui/routes/admin.py
+++ app/gui/routes/admin.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 
 from app.gui.routes.templating import get_templating_engine
+from app.i18n.get_request_locale import get_request_locale
 
 I18N_DOMAIN = "admin"
 
@@ -10,26 +11,29 @@ router = APIRouter()
 @router.get("")
 async def overview(request: Request):
     """Return the overview page in the admin gui"""
-    return get_templating_engine("admin", request.state.locale).TemplateResponse(
-        "index.html.jinja", {"request": request, "page": "overview", "locale": request.state.locale}
+    return get_templating_engine("admin", get_request_locale(request)).TemplateResponse(
+        "index.html.jinja",
+        {"request": request, "page": "overview", "locale": get_request_locale(request)},
     )
 
 
 @router.get("/retrieve")
 async def get_retrieve(request: Request):
     """Return the retrieve page in the admin gui"""
-    return get_templating_engine("admin", request.state.locale).TemplateResponse(
+    return get_templating_engine("admin", get_request_locale(request)).TemplateResponse(
         "retrieve.html.jinja",
-        {"request": request, "page": "retrieve", "locale": request.state.locale}
+        {"request": request, "page": "retrieve", "locale": get_request_locale(request)},
     )
 
 
 @router.get("/history")
 async def get_history(request: Request):
     """Return the history page in the admin gui"""
-    return get_templating_engine(I18N_DOMAIN, request.state.locale).TemplateResponse(
+    return get_templating_engine(
+        I18N_DOMAIN, get_request_locale(request)
+    ).TemplateResponse(
         "history.html.jinja",
-        {"request": request, "page": "history", "locale": request.state.locale}
+        {"request": request, "page": "history", "locale": get_request_locale(request)},
     )
 
 
