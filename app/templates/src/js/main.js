0abc4731153f8c7d761355751e1d6d4df8b77b0d
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2023-10-03 09:29:54 +0200

    84 add gui controls (#85)
    * Safe mode toggle
    
    * Add tootips + refacto
    
    * Use tom select
    
    * Add datepicker

--- app/templates/src/js/main.js
+++ app/templates/src/js/main.js
@@ -4,6 +4,32 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-
+import 'bootstrap5-toggle/js/bootstrap5-toggle.ecmas.min'
+import 'tom-select/dist/js/tom-select.complete.min'
 // Import our custom JS
-import './index.js'
+import env from "./env"
+import Client from "./common/client"
+import RetrievePage from "./retrieve/retrieve_page"
+
+const enableTooltips = function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+}
+
+const init = function () {
+    enableTooltips();
+    const pageIdentifier = document.getElementById("page-identifier").value;
+    switch (pageIdentifier) {
+        case "overview":
+            break;
+        case "retrieve":
+            const client = new Client(env);
+            new RetrievePage(env, client, document.getElementById("retrieve-page-content"));
+            break;
+        case "history":
+            break;
+    }
+
+}
+document.addEventListener("DOMContentLoaded", init);
