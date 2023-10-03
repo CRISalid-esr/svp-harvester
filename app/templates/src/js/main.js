0476b5632c1129ec2ee1545ca9bdc4e09fa1d3e5
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-10-03 08:01:13 +0200

    Add tootips + refacto

--- app/templates/src/js/main.js
+++ app/templates/src/js/main.js
@@ -7,4 +7,29 @@ import * as bootstrap from 'bootstrap'
 import 'bootstrap5-toggle/js/bootstrap5-toggle.ecmas.min'
 
 // Import our custom JS
-import './index.js'
+import env from "./env"
+import Client from "./common/client"
+import RetrievePage from "./retrieve/retrieve_page"
+
+const enableTooltips = function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+}
+
+const init = function () {
+    enableTooltips();
+    const pageIdentifier = document.getElementById("page-identifier").value;
+    switch (pageIdentifier) {
+        case "overview":
+            break;
+        case "retrieve":
+            const client = new Client(env);
+            new RetrievePage(env, client, document.getElementById("retrieve-page-content"));
+            break;
+        case "history":
+            break;
+    }
+
+}
+document.addEventListener("DOMContentLoaded", init);
