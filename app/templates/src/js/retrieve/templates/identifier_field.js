78b3f711461847c220c74bfdf5801e7b9e1ee945
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2023-10-11 10:06:58 +0200

    93 checkbox to cancel identifier (#107)
    * Handle identifier nullification

--- app/templates/src/js/retrieve/templates/identifier_field.js
+++ app/templates/src/js/retrieve/templates/identifier_field.js
@@ -1,10 +1,10 @@
 const identifier_field = `<div class="input-group mt-3 mb-3 identifier-field-container">
                     <div style="width: max-content;">
-                        <select disabled class="form-select rounded-end-0" aria-label="Example select with button addon">
+                        <select disabled class="form-select rounded-end-0 identifier-control-select" aria-label="Example select with button addon">
                             <option selected value="<%= identifierType %>"><%= identifierLabel %></option>
                         </select>
                     </div>
-                    <input type="text" class="form-control rounded-0" disabled value="<%= identifierValue %>">                 
+                    <input type="text" class="form-control rounded-0 identifier-control-input" disabled value="<%= identifierValue %>">                 
                     <button type="button" class="btn btn-success rounded-start-0  rounded-end-0 btn-validate-identifier d-none"><i class="bi bi-check-circle"></i></button>                  
                     <button type="button" class="btn btn-warning rounded-start-0  rounded-end-0 btn-edit-identifier"><i class="bi bi-pencil"></i></button>                  
                     <button type="button" class="btn btn-danger rounded-start-0 btn-remove-identifier"><i class="bi bi-x-circle"></i></button>                  
