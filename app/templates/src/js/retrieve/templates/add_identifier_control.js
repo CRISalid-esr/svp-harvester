127c92d8487fc91d07428bf0b6b3fbb84c6cc396
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-10-11 07:55:04 +0200

    Handle void value client side

--- app/templates/src/js/retrieve/templates/add_identifier_control.js
+++ app/templates/src/js/retrieve/templates/add_identifier_control.js
@@ -1,14 +1,14 @@
 const add_identifier_control = `
 <div class="input-group mt-3 mb-3" id="add-identifier-control">
                     <div style="width: max-content;">
-                        <select class="form-select rounded-end-0" aria-label="Example select with button addon" id="add-identifier-control-select">
+                        <select class="form-select rounded-end-0 identifier-control-select" aria-label="Example select with button addon" id="add-identifier-control-select">
                             <option selected disabled value="">Sélectionnez un type d'identifiant</option>
                             <% Object.entries(identifiers).forEach(function(identifier){ %>
                                 <option value="<%= identifier[0] %>"><%= identifier[1]['label'] %></option>
                             <% }); %>
                         </select>
                     </div>
-                    <input type="text" class="form-control rounded-0" id="add-identifier-control-input">
+                    <input type="text" class="form-control rounded-0 identifier-control-input" id="add-identifier-control-input">
                     <button type="button" class="btn btn-success rounded-start-0" id="add-identifier-control-button"><i class="bi bi-plus-circle"></i></button>
                 </div>
 `
