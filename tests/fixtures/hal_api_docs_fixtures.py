4be3d0c280dd9c8540e8ff5e76facde692490c71
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-10-15 05:41:04 +0200

    Add typing to hal_api_docs fixtures

--- tests/fixtures/hal_api_docs_fixtures.py
+++ tests/fixtures/hal_api_docs_fixtures.py
@@ -4,7 +4,7 @@ from tests.fixtures.common import _json_data_from_file
 
 
 @pytest.fixture(name="hal_api_docs_for_one_researcher")
-def fixture_hal_api_docs_for_one_researcher(_base_path):
+def fixture_hal_api_docs_for_one_researcher(_base_path) -> dict:
     """
     Generate a HAL API response for one researcher in JSON format
     :param _base_path: test data directory base
@@ -14,7 +14,7 @@ def fixture_hal_api_docs_for_one_researcher(_base_path):
 
 
 @pytest.fixture(name="hal_api_docs_with_same_kw_twice")
-def fixture_hal_api_docs_with_same_kw_twice(_base_path):
+def fixture_hal_api_docs_with_same_kw_twice(_base_path) -> dict:
     """
     Generate a HAL API response for one researcher in JSON format
     :param _base_path: test data directory base
@@ -23,6 +23,6 @@ def fixture_hal_api_docs_with_same_kw_twice(_base_path):
     return _hal_api_json_data_from_file(_base_path, "docs_with_same_kw_twice")
 
 
-def _hal_api_json_data_from_file(base_path, file_name):
+def _hal_api_json_data_from_file(base_path, file_name) -> dict:
     file_path = f"data/hal_api/{file_name}.json"
     return _json_data_from_file(base_path, file_path)
