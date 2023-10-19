43bf29ac491676a93937e7834c0cf88bbff5331d
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-10-19 18:16:29 +0200

    Launch Idref harvester with orcid and test

---
+++ tests/test_harvesters/test_idref/test_data_idref_fr_sparql_query_builder.py
@@ -0,0 +1,52 @@
+""" Test for the hal api query builder"""
+
+import pytest
+
+from app.harvesters.idref.idref_sparql_query_builder import IdrefSparqlQueryBuilder
+
+
+def test_build_query_without_subject_type_raises_error():
+    """
+    GIVEN a HalApiQueryBuilder instance
+    WHEN the subject type is not set
+    THEN an AssertionError is raised
+
+    :return: None
+    """
+    idref_query_builder = IdrefSparqlQueryBuilder()
+    with pytest.raises(AssertionError):
+        idref_query_builder.build()
+
+@pytest.mark.current
+def test_build_query_for_person_with_idref():
+    """
+    GIVEN a HalApiQueryBuilder instance
+    WHEN the idref parameter is set
+    THEN the query filter is in the query
+
+    :return: None
+    """
+    idref_query_builder = IdrefSparqlQueryBuilder()
+    idref_query_builder.set_subject_type(IdrefSparqlQueryBuilder.SubjectType.PERSON)
+    idref_id = "test_idref_id"
+    idref_query_builder.set_idref_id(idref_id)
+    assert idref_query_builder.subject_uri == f"http://www.idref.fr/{idref_id}/id"
+    query = idref_query_builder.build()
+    assert f"?pub ?role <http://www.idref.fr/{idref_id}/id> ." in query
+
+
+def test_build_query_for_person_with_orcid():
+    """
+    GIVEN a HalApiQueryBuilder instance
+    WHEN the orcid parameter is set
+    THEN the query filter is in the query
+
+    :return: None
+    """
+    idref_query_builder = IdrefSparqlQueryBuilder()
+    idref_query_builder.set_subject_type(IdrefSparqlQueryBuilder.SubjectType.PERSON)
+    orcid = "test_orcid"
+    idref_query_builder.set_orcid(orcid)
+    query = idref_query_builder.build()
+    assert "?pub ?role ?pers ." in query
+    assert f"?pers vivo:orcidId \"{orcid}\" ." in query
--- tests/test_harvesters/test_idref/test_idref_harvester.py
+++ tests/test_harvesters/test_idref/test_idref_harvester.py
@@ -4,7 +4,13 @@ from app.harvesters.idref.idref_references_converter import IdrefReferencesConve
 from app.models.people import Person
 
 
-def test_idref_harvester_relevant(person_with_name_and_idref: Person):
+def test_idref_harvester_relevant_with_idref(person_with_name_and_idref: Person):
     """Test that the harvester will run if submitted with an IDREF."""
     harvester = IdrefHarvester(converter=IdrefReferencesConverter())
     assert harvester.is_relevant(person_with_name_and_idref) is True
+
+
+def test_idref_harvester_relevant_with_orcid(person_with_name_and_orcid: Person):
+    """Test that the harvester will run if submitted with an ORCID."""
+    harvester = IdrefHarvester(converter=IdrefReferencesConverter())
+    assert harvester.is_relevant(person_with_name_and_orcid) is True
