b596b4a3e8c6ddaa78041c02cbf7a15f0910b51f
E-Bara <62282795+E-Bara@users.noreply.github.com>
2023-11-13 10:44:59 +0100

    150 fetch abstracts from scienceplus endpoint (#157)
    * Normalize science_plus_references_converter _titles method
    
    * Add abstract fetching in science plus converter
    
    * Add test for science_plus_references_converter.py
    
    * Update documentation description

---
+++ tests/test_harvesters/test_idref/test_science_plus_references_converter.py
@@ -0,0 +1,36 @@
+import pytest
+
+from app.harvesters.idref.science_plus_references_converter import SciencePlusReferencesConverter
+
+
+@pytest.mark.current
+@pytest.mark.asyncio
+async def test_science_plus_convert_for_rdf_result(
+    science_plus_rdf_result_for_doc,
+):
+    """
+    GIVEN a SudocReferencesConverter instance and a Sudoc RDF result
+    WHEN the convert method is called
+    THEN it should return a Reference instance with the expected values
+
+    :param sudoc_rdf_result_for_doc: sudoc RDF result for a document
+    :return: None
+    """
+    converter_under_tests = SciencePlusReferencesConverter()
+    expected_french_title = (
+        "Marges urbaines, marges rurales entre Santiago du Chili et Valparaíso"
+    )
+    expected_french_abstract_beginning = (
+        "La Cordillère de la Côte, entre Santiago du Chili et Valparaíso a vu son accessibilité "
+    )
+    result = await converter_under_tests.convert(science_plus_rdf_result_for_doc)
+    assert result.source_identifier == str(science_plus_rdf_result_for_doc.source_identifier)
+    # assert that there are two titles
+    assert len(result.titles) == 2
+    # assert that the expected french title is one of the titles at any position
+    assert expected_french_title in [title.value for title in result.titles]
+    # expect that one of the abstacts begins with the expected french abstract beginning
+    assert any(
+        abstract.value.startswith(expected_french_abstract_beginning)
+        for abstract in result.abstracts
+    )
