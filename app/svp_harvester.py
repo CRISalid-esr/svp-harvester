bbf7e35d2ca9ff534a4face252892cb68d6a27eb
Paldana99 <pablo.a@galadrim.fr>
2024-01-18 18:09:40 +0100

    chore: logger with environments

--- app/svp_harvester.py
+++ app/svp_harvester.py
@@ -1,5 +1,4 @@
 import asyncio
-import sys
 
 from aiormq import AMQPConnectionError
 from fastapi import FastAPI
@@ -38,10 +37,13 @@ class SvpHarvester(FastAPI):
 
         self.include_router(gui_router)
 
+        # Remove default logger and add custom logger
+        logger.remove()
         logger.add(
-            sys.stderr,
-            format="Log : [{extra[log_id]}]:{time} - {level} - {message}",
+            settings.logger_sink,
+            # format="Log : {time} - {level} - {message}",
             level=settings.loguru_level,
+            # rotation="100 MB",
         )
 
         self.add_exception_handler(ValidationError, http422_error_handler)
@@ -49,13 +51,15 @@ class SvpHarvester(FastAPI):
             self.add_event_handler("startup", self.open_rabbitmq_connexion)
             self.add_event_handler("shutdown", self.close_rabbitmq_connexion)
 
+    @logger.catch
     async def open_rabbitmq_connexion(self) -> None:  # pragma: no cover
         """Init AMQP connexion at boot time"""
         try:
-            print("Enabling RabbitMQ connexion")
+            logger.info("Enabling RabbitMQ connexion")
             self.amqp_interface = AMQPInterface(get_app_settings())
             asyncio.create_task(self.amqp_interface.listen(), name="amqp_listener")
-            print("RabbitMQ connexion has been enabled")
+            logger.info("RabbitMQ connexion has been enabled")
+            # raise RuntimeError("test")
         except AMQPConnectionError as error:
             raise RuntimeError(
                 "Cannot connect to RabbitMQ, please check your configuration"
@@ -67,6 +71,6 @@ class SvpHarvester(FastAPI):
 
     async def close_rabbitmq_connexion(self) -> None:  # pragma: no cover
         """Handle last tasks before shutdown"""
-        print("Shutting down RabbitMQ connexion")
+        logger.info("Closing RabbitMQ connexion")
         await self.amqp_interface.stop_listening()
-        print("RabbitMQ connexion has been closed")
+        logger.info("RabbitMQ connexion has been closed")
