6fcf78501209bf90dc261d3664d39b173feaf8b7
Pablo Aldana <pablo.aldana@uc.cl>
2024-01-18 10:11:00 +0100

    Feat Document Type model (#198)
    * Feat Document Type model
    
    * Fix document-type label can be none
    
    * fix: put in plural, change code to uri
    
    ---------
    
    Co-authored-by: Paldana99 <pablo.a@galadrim.fr>

---
+++ app/models/document_type.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class DocumentType(BaseModel):
+    """
+    Pydantic model matching DocumentType sql_alchemy model
+    """
+
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+
+    uri: str
+
+    label: str | None
