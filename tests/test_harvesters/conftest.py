e091169e1d62302d003d3b7f2bb1ebfb5b0bc778
Paldana99 <pablo.a@galadrim.fr>
2024-01-19 15:10:30 +0100

    test: Add test with log and restore conftest

--- tests/test_harvesters/conftest.py
+++ tests/test_harvesters/conftest.py
@@ -1,6 +1,8 @@
 from unittest import mock
+from loguru import logger
 
 import pytest
+from pytest import LogCaptureFixture
 
 from app.db.references.references_recorder import ReferencesRecorder
 
@@ -12,3 +14,25 @@ def fixture_reference_recorder_register_mock():
         ReferencesRecorder, "register_creation"
     ) as reference_recorder_register_mock:
         yield reference_recorder_register_mock
+
+
+@pytest.fixture(autouse=True)
+def caplog(caplog: LogCaptureFixture):  # pylint: disable=redefined-outer-name
+    """
+    Make pytest work with loguru. See:
+    https://loguru.readthedocs.io/en/stable/resources/migration.html#making-things-work-with-pytest-and-caplog
+    :param caplog: pytest fixture
+    :return: loguru compatible caplog
+    """
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False,
+    )
+    yield caplog
+    try:
+        logger.remove(handler_id)
+    except ValueError:
+        pass
