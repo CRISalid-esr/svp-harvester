794f1e4f095b7687f0db92d0f76fbb4dca2598d4
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2024-01-20 09:46:53 +0100

    Add UML model generator and display result in rst documentation (#212)

---
+++ scripts/generate_uml_diagram.py
@@ -0,0 +1,33 @@
+# may require `apt install graphviz` on linux terminals
+from app.db.session import Base
+from app.db.models import (
+    abstract,
+    affiliations,
+    concept,
+    contribution,
+    contributor,
+    document_type,
+    entity,
+    harvesting,
+    identifier,
+    label,
+    literal_field,
+    organization,
+    person,
+    reference,
+    reference_event,
+    references_document_type,
+    references_subject,
+    retrieval,
+    subtitle,
+    title,
+    versioned_record,
+)
+from sqlalchemy_schemadisplay import create_uml_graph
+
+graph = create_uml_graph(
+    Base.registry.mappers,
+    show_operations=False,
+    show_multiplicity_one=True,
+)
+graph.write_png("../img/sql_alchemy_entities.png")
