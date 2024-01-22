1afcd20943506a3698cf45656832660d10b08457
E-Bara <62282795+E-Bara@users.noreply.github.com>
2024-01-22 12:52:04 +0100

    Handle contributors

--- app/harvesters/scanr/scanr_references_converter.py
+++ app/harvesters/scanr/scanr_references_converter.py
@@ -1,10 +1,21 @@
+import re
+from typing import AsyncGenerator
+
+from app.db.daos.contributor_dao import ContributorDAO
 from app.db.models.abstract import Abstract
 from app.db.models.reference import Reference
 from app.db.models.title import Title
+from app.db.models.contribution import Contribution
+from app.db.session import async_session
+from app.harvesters.scanr.scanr_qualities_converter import ScanrQualitiesConverter
 from app.harvesters.abstract_references_converter import AbstractReferencesConverter
 from app.harvesters.json_harvester_raw_result import (
     JsonHarvesterRawResult as JsonRawResult,
 )
+from app.harvesters.exceptions.unexpected_format_exception import (
+    UnexpectedFormatException,
+)
+from app.db.models.contributor import Contributor
 
 
 class ScanrReferencesConverter(AbstractReferencesConverter):
@@ -24,28 +35,98 @@ class ScanrReferencesConverter(AbstractReferencesConverter):
 
         title = json_payload["_source"].get("title")
         if title:
-            new_ref.titles.extend(self._remove_duplicates_from_language_data(title, Title))
+            new_ref.titles.extend(
+                self._remove_duplicates_from_language_data(title, Title)
+            )
 
         summary = json_payload["_source"].get("summary")
         if summary:
-            new_ref.abstracts.extend(self._remove_duplicates_from_language_data(summary, Abstract))
+            new_ref.abstracts.extend(
+                self._remove_duplicates_from_language_data(summary, Abstract)
+            )
+
+        contributions = json_payload["_source"].get("authors")
+        if contributions:
+            async for contribution in self._contributions(contributions):
+                new_ref.contributions.append(contribution)
 
         new_ref.hash = self._hash(json_payload)
         new_ref.harvester = "scanR"
         new_ref.source_identifier = json_payload["_source"].get("id")
         return new_ref
 
-    def _remove_duplicates_from_language_data(self, language_data: dict, model_class):
+    async def _contributions(self, contributions) -> AsyncGenerator[Contribution, None]:
+        async with async_session() as session:
+            for rank, contribution in enumerate(contributions):
+                quality = contribution.get("role")
+                name = contribution.get("fullName")
+                contributor = contribution.get("person")
 
+                ids = None
+                if contributor is not None:
+                    if not re.match(
+                        r"idref\d+", contributor
+                    ):  # TODO: number of digits in IDREF? 9?
+                        raise UnexpectedFormatException(
+                            f"Unexpected format for contributor {contributor}"
+                        )
+                    ids = contributor.replace("idref", "")
+                    # TODO: Should we use contributor_id or contributor?
+                    #  We use Idref for the search but Scanr use a variation "idref1234" as identifier
+
+                    db_contributor = await ContributorDAO(
+                        session
+                    ).get_by_source_and_identifier(
+                        source="idref", source_identifier=ids
+                    )
+                else:
+                    db_contributor = await ContributorDAO(
+                        session
+                    ).get_by_source_and_name(source="idref", name=name)
+
+                if db_contributor is None:
+                    db_contributor = Contributor(
+                        source="idref",
+                        source_identifier=ids,
+                        name=name,
+                    )
+                else:
+                    self._update_contributor_name(db_contributor, name)
+
+                yield Contribution(
+                    contributor=db_contributor,
+                    role=ScanrQualitiesConverter.convert(quality=quality),
+                    rank=rank,
+                )
+
+    def _update_contributor_name(self, db_contributor: Contributor, name: str):
+        """
+        Updates the name of the contributor if it is different from the one in the database
+        and stores the old name in the name_variants field
+
+        :param db_contributor:
+        :param name: new name received from hal
+        :return: None
+        """
+        if db_contributor.name == name:
+            return
+        if db_contributor.name not in db_contributor.name_variants:
+            # with append method sqlalchemy would not detect the change
+            db_contributor.name_variants = db_contributor.name_variants + [
+                db_contributor.name
+            ]
+        db_contributor.name = name
+
+    def _remove_duplicates_from_language_data(self, language_data: dict, model_class):
         processed_items = [
             model_class(value=value, language=key)
-            for key, value in language_data.items() if key != "default"
+            for key, value in language_data.items()
+            if key != "default"
         ]
 
         default_item = language_data.get("default")
 
         if default_item:
-
             if not any(item for item in processed_items if item.value == default_item):
                 processed_items.append(model_class(value=default_item, language=None))
 
---
+++ app/harvesters/scanr/scanr_utilities_get_unique_values.py
@@ -0,0 +1,44 @@
+from elasticsearch import Elasticsearch, ElasticsearchException
+
+from app.config import get_app_settings
+
+try:
+    app_settings = get_app_settings()
+
+    # Create an Elasticsearch instance
+    es = Elasticsearch(
+        [app_settings.scanr_es_host],
+        http_auth=(app_settings.scanr_es_user, app_settings.scanr_es_password),
+        use_ssl=True,
+        verify_certs=True,
+        scheme="https",
+    )
+except ElasticsearchException as e:
+    print(f"An error occurred while connecting to Elasticsearch: {e}")
+
+
+def get_unique_values(selected_field: str = "authors.role", size: int = 100):
+    aggregation_query = {
+        "size": 0,
+        "aggs": {
+            "unique_values": {
+                "terms": {"field": f"{selected_field}.keyword", "size": size}
+            }
+        },
+    }
+
+    # Perform the search using the aggregation query
+    response = es.search(index="scanr-publications-staging", body=aggregation_query)
+
+    # Access the aggregation results
+    aggregation_results = response["aggregations"]["unique_values"]["buckets"]
+    print(f"Number of unique occurrences: {len(aggregation_results)}")
+    print(aggregation_results)
+    keys = [bucket["key"] for bucket in aggregation_results]
+    print(keys)
+
+
+if __name__ == "__main__":
+    get_unique_values()
+    get_unique_values(selected_field="type")
+    get_unique_values(selected_field="productionType")
