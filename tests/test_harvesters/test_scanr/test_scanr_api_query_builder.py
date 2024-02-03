e383e24985d6514748294ab0bc49feb72d561948
E-Bara <62282795+E-Bara@users.noreply.github.com>
2024-02-03 10:50:15 +0100

    225 add support to halid_s and orcid for scanr harvester (#262)
    * Refacto set_publication_query
    
    * Add support to orcid & idhal_s
    
    * Add test case when identifier is not idref
    
    * Update Scanr harvester documentation

--- tests/test_harvesters/test_scanr/test_scanr_api_query_builder.py
+++ tests/test_harvesters/test_scanr/test_scanr_api_query_builder.py
@@ -11,111 +11,95 @@ def fixture_scanr_api_query_builder():
     yield scanr_query
 
 
-def test_set_subject_type(scanr_query_builder):
-    """Test if the set_subject_type function return the values set"""
-    scanr_query_builder.set_subject_type(scanr_query_builder.SubjectType.PERSON)
-
-    assert scanr_query_builder.subject_type.value == "person"
-
-
 def test_set_query(scanr_query_builder):
     """Test if the set_query function return the values set"""
-    scanr_query_builder.set_query(
-        scanr_query_builder.QueryParameters.AUTH_IDREF, "2556"
-    )
+    scanr_query_builder.set_publication_query("idref09850")
 
-    assert scanr_query_builder.identifier_type.value == "idref"
-    assert scanr_query_builder.identifier_value == "2556"
+    assert scanr_query_builder.scanr_id == "idref09850"
 
 
 def test_build_person_query_with_idref(scanr_query_builder):
     """
-                GIVEN a ScanRApiQueryBuilder instance
-                WHEN the build function is called with the identifier type set on AUTH_IDREF
-                THEN the query contains a filter with the "id" parameter
+    GIVEN a ScanRApiQueryBuilder instance
+    WHEN the build function is called with the identifier type set on AUTH_IDREF
+    THEN the query contains a filter with the "id" parameter
 
-                :param scanr_query_builder:
-                :return:
-                """
-    scanr_query_builder.set_subject_type(scanr_query_builder.SubjectType.PERSON)
-    scanr_query_builder.set_query(
-        scanr_query_builder.QueryParameters.AUTH_IDREF, "2556"
+    :param scanr_query_builder:
+    :return:
+    """
+    scanr_query_builder.set_person_query(
+        scanr_query_builder.QueryParameters.AUTH_IDREF, "idref2556"
     )
     query = scanr_query_builder.build()
 
     expected_result = {
-        '_source': ['id', 'externalIds', 'domains', 'affiliations', 'fullName'],
-        'query': {
-            'bool': {
-                'must': [{'term': {'id': 'idref2556'}}]
+        "_source": ["id", "externalIds", "domains", "affiliations", "fullName"],
+        "query": {
+            "bool": {
+                "must": [
+                    {"term": {"externalIds.type.keyword": "idref"}},
+                    {"term": {"externalIds.id.keyword": "idref2556"}},
+                ]
             }
         },
-        'sort': {
-            'publicationDate': {'order': 'desc'}
-        }
     }
     assert query == expected_result
 
 
 def test_build_person_query_with_orcid(scanr_query_builder):
     """
-            GIVEN a ScanRApiQueryBuilder instance
-            WHEN the build function is called with the identifier type set on AUTH_ORCID
-            THEN the query contains a filter with the "orcid" parameter
+    GIVEN a ScanRApiQueryBuilder instance
+    WHEN the build function is called with the identifier type set on AUTH_ORCID
+    THEN the query contains a filter with the "orcid" parameter
 
-            :param scanr_query_builder:
-            :return:
-            """
-    scanr_query_builder.set_subject_type(scanr_query_builder.SubjectType.PERSON)
-    scanr_query_builder.set_query(
-        scanr_query_builder.QueryParameters.AUTH_ORCID, "2556"
+    :param scanr_query_builder:
+    :return:
+    """
+    scanr_query_builder.set_person_query(
+        scanr_query_builder.QueryParameters.AUTH_ORCID, "idref2556"
     )
     query = scanr_query_builder.build()
 
     expected_result = {
-        '_source': ['id', 'externalIds', 'domains', 'affiliations', 'fullName'],
-        'query': {
-            'bool': {
-                'must': [{'term': {'orcid': '2556'}}]
+        "_source": ["id", "externalIds", "domains", "affiliations", "fullName"],
+        "query": {
+            "bool": {
+                "must": [
+                    {"term": {"externalIds.type.keyword": "orcid"}},
+                    {"term": {"externalIds.id.keyword": "idref2556"}},
+                ]
             }
         },
-        'sort': {
-            'publicationDate': {'order': 'desc'}
-        }
     }
     assert query == expected_result
 
 
 def test_build_person_query_without_idref_or_orcid(scanr_query_builder):
     """
-        GIVEN a ScanRApiQueryBuilder instance
-        WHEN the build function is called with the identifier type
-        not set on AUTH_IDREF or AUTH_ORCID
-        THEN the query contains a filter with the "externalIds.type.keyword"
-        and "externalIds.id.keyword" parameters
+    GIVEN a ScanRApiQueryBuilder instance
+    WHEN the build function is called with the identifier type
+    not set on AUTH_IDREF or AUTH_ORCID
+    THEN the query contains a filter with the "externalIds.type.keyword"
+    and "externalIds.id.keyword" parameters
 
-        :param scanr_query_builder:
-        :return:
-        """
-    scanr_query_builder.set_subject_type(scanr_query_builder.SubjectType.PERSON)
-    scanr_query_builder.set_query(
+    :param scanr_query_builder:
+    :return:
+    """
+    scanr_query_builder.set_person_query(
         scanr_query_builder.QueryParameters.AUTH_ID_HAL_S, "john-doe"
     )
     query = scanr_query_builder.build()
 
     expected_result = {
-        '_source': ['id', 'externalIds', 'domains', 'affiliations', 'fullName'],
-        'query': {
-            'bool': {
-                'must': [
-                    {'term': {'externalIds.type.keyword': 'id_hal'}},
-                    {'term': {'externalIds.id.keyword': 'john-doe'}}
+        "_source": ["id", "externalIds", "domains", "affiliations", "fullName"],
+        "query": {
+            "bool": {
+                "must": [
+                    {"term": {"externalIds.type.keyword": "id_hal"}},
+                    {"term": {"externalIds.id.keyword": "john-doe"}},
                 ]
             }
         },
-        'sort': {
-            'publicationDate': {'order': 'desc'}
-        }
     }
     assert query == expected_result
 
@@ -129,52 +113,24 @@ def test_build_publication_query_with_idref(scanr_query_builder):
     :param scanr_query_builder:
     :return:
     """
-    scanr_query_builder.set_subject_type(scanr_query_builder.SubjectType.PUBLICATION)
-    scanr_query_builder.set_query(
-        scanr_query_builder.QueryParameters.AUTH_IDREF, "2556"
-    )
+    scanr_query_builder.set_publication_query("idref2556")
     query = scanr_query_builder.build()
 
     expected_result = {
-        '_source': [
-            'id',
-            'title',
-            'summary',
-            'type',
-            'productionType',
-            'publicationDate',
-            'domains',
-            'affiliations',
-            'authors',
-            'externalIds'
+        "_source": [
+            "id",
+            "title",
+            "summary",
+            "type",
+            "productionType",
+            "publicationDate",
+            "domains",
+            "affiliations",
+            "authors",
+            "externalIds",
         ],
-        'query': {
-            'bool': {
-                'must': [{'term': {'authors.person': 'idref2556'}}]
-            }
-        },
-        'sort': {
-            'publicationDate': {'order': 'desc'}
-        }
+        "query": {"bool": {"must": [{"term": {"authors.person": "idref2556"}}]}},
+        "sort": {"publicationDate": {"order": "desc"}},
     }
 
     assert query == expected_result
-
-
-def test_build_publication_query_without_idref(scanr_query_builder):
-    """Test if build function raise an error when publication build is called without idref"""
-    scanr_query_builder.set_subject_type(scanr_query_builder.SubjectType.PUBLICATION)
-    scanr_query_builder.set_query(
-        scanr_query_builder.QueryParameters.AUTH_ORCID, "2556"
-    )
-
-    with pytest.raises(NotImplementedError):
-        scanr_query_builder.build()
-
-
-def test_build_without_set_query(scanr_query_builder):
-    """Test if build function raise an error when set_query is not set"""
-    scanr_query_builder.set_subject_type(scanr_query_builder.SubjectType.PERSON)
-
-    with pytest.raises(AssertionError):
-        scanr_query_builder.build()
--- tests/test_harvesters/test_scanr/test_scanr_harvester.py
+++ tests/test_harvesters/test_scanr/test_scanr_harvester.py
@@ -16,25 +16,43 @@ def test_scanr_harvester_relevant_with_idref(person_with_name_and_idref: Person)
     assert harvester.is_relevant(person_with_name_and_idref) is True
 
 
-def test_scanr_harvester_not_relevant_without_idref(person_with_name_and_orcid: Person):
+def test_scanr_harvester_relevant_with_orcid(person_with_name_and_orcid: Person):
     """Test that the harvester will not run if not submitted with an IDREF"""
     harvester = ScanrHarvester(converter=ScanrReferencesConverter())
-    assert harvester.is_relevant(person_with_name_and_orcid) is False
+    assert harvester.is_relevant(person_with_name_and_orcid) is True
+
+
+def test_scanr_harvester_not_relevant_with_halid_s(
+    person_with_name_and_id_hal_s: Person,
+):
+    """Test that the harvester will not run if not submitted with an IDREF"""
+    harvester = ScanrHarvester(converter=ScanrReferencesConverter())
+    assert harvester.is_relevant(person_with_name_and_id_hal_s) is True
+
+
+def test_scanr_harvester_not_relevant_with_person_without_identifiers(
+    person_without_identifiers: Person,
+):
+    """Test that the harvester will not run if not submitted with an IDREF"""
+    harvester = ScanrHarvester(converter=ScanrReferencesConverter())
+    assert harvester.is_relevant(person_without_identifiers) is False
 
 
 @pytest.mark.asyncio
-async def test_fetch_references_async(
-        test_client: TestClient,
-        person_with_name_and_idref_json,
-        scanr_api_docs_from_publication,
+async def test_fetch_references_async_with_idref(
+    test_client: TestClient,
+    person_with_name_and_idref_json,
+    scanr_api_docs_from_publication,
 ):
     """
-    Test a post request with history safe mode set to False.
+    Test a post request with history safe mode set to False and a person with an IDREF
     """
 
-    with (mock.patch.object(AsyncElasticsearch, 'search',
-                            new=mock.AsyncMock(return_value=scanr_api_docs_from_publication)
-                            )):
+    with mock.patch.object(
+        AsyncElasticsearch,
+        "search",
+        new=mock.AsyncMock(return_value=scanr_api_docs_from_publication),
+    ):
         response = test_client.post(
             REFERENCES_RETRIEVAL_API_PATH,
             json={
@@ -42,7 +60,54 @@ async def test_fetch_references_async(
                 "events": ["created", "updated", "deleted", "unchanged"],
                 "history_safe_mode": False,
                 "harvesters": ["scanr"],
-            }
+            },
+        )
+
+        assert response.status_code == 200
+        json_response = response.json()
+        retrieval_url = json_response["retrieval_url"]
+        assert retrieval_url is not None
+
+        response = test_client.get(retrieval_url)
+        assert response.status_code == 200
+        json_response = response.json()
+        assert json_response["harvestings"][0]["harvester"] == "scanr"
+        assert json_response["harvestings"][0]["state"] == "completed"
+        assert len(json_response["harvestings"][0]["reference_events"]) == 1
+
+        assert all(
+            reference_event["type"] == "created"
+            for reference_event in json_response["harvestings"][0]["reference_events"]
+        )
+
+
+@pytest.mark.asyncio
+async def test_fetch_references_async_with_orcid(
+    test_client: TestClient,
+    person_with_name_and_orcid_json,
+    scanr_api_docs_from_publication,
+):
+    """
+    Test a post request with history safe mode set to False and with an ORCID.
+    """
+
+    with mock.patch.object(
+        AsyncElasticsearch,
+        "search",
+        new=mock.AsyncMock(return_value=scanr_api_docs_from_publication),
+    ), mock.patch.object(
+        ScanrHarvester,
+        "_get_entity_scanr_id",
+        new=mock.AsyncMock(return_value="idref123456789"),
+    ):
+        response = test_client.post(
+            REFERENCES_RETRIEVAL_API_PATH,
+            json={
+                "person": person_with_name_and_orcid_json,
+                "events": ["created", "updated", "deleted", "unchanged"],
+                "history_safe_mode": False,
+                "harvesters": ["scanr"],
+            },
         )
 
         assert response.status_code == 200
