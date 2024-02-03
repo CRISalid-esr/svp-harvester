e689100c588edc65edd49531a0afe64ddacef4d2
E-Bara <62282795+E-Bara@users.noreply.github.com>
2024-02-03 10:43:11 +0100

    230 index for abstract (#257)
    * Remove abstract from inheritance
    
    * Update table inheritance to concrete

--- app/db/models/abstract.py
+++ app/db/models/abstract.py
@@ -1,17 +1,14 @@
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy import Text
+from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from app.db.models.reference_literal_field import ReferenceLiteralField
 
 
 class Abstract(ReferenceLiteralField):
-    """
-    Model for persistence of titles
-    """
+    """Model for persistence of abstracts"""
 
-    __mapper_args__ = {
-        "polymorphic_identity": "abstract",
-    }
+    __tablename__ = "abstracts"
+    __mapper_args__ = {"concrete": True}
 
-    reference: Mapped["app.db.models.reference.Reference"] = relationship(
-        "app.db.models.reference.Reference", back_populates="abstracts", lazy="raise"
-    )
+    value: Mapped[str] = mapped_column(Text, nullable=False, index=False)
+    reference = relationship("Reference", back_populates="abstracts", lazy="raise")
