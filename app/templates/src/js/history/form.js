59c7e97b1f1feddbbf3caaecf88fba3734d700f7
Paldana99 <pablo.a@galadrim.fr>
2024-02-05 17:31:05 +0100

    feat: history pages (collections, publications)

--- app/templates/src/js/history/form.js
+++ app/templates/src/js/history/form.js
@@ -1,7 +1,12 @@
 import TextFieldSelector from "../common/text_field_selector";
 import TomSelect from "tom-select";
 import DateRangePicker from "vanillajs-datepicker/DateRangePicker";
+import add_identifier_control from "../retrieve/templates/add_identifier_control";
+import identifier_field from "../retrieve/templates/identifier_field";
+import stringToHTML from "../utils";
 import th from "vanillajs-datepicker/locales/th";
+
+const IDENTIFIER_NULL_VALUE = "null";
 
 class HistoryForm {
     constructor(env, rootElement, subpage) {
@@ -9,10 +14,12 @@ class HistoryForm {
         this.rootElement = rootElement;
         this.subpage = subpage
         this.formElement = rootElement.querySelector("#form-element");
+        this.identifierFieldsContainer = this.formElement.querySelector("#identifier-fields-container");
+        this.runSearchButton = this.formElement.querySelector("#run-history-collection-btn");
         this.handleEventTypesSelect();
         this.handleDatePickers();
         this.handleDataSourcesSelect();
-
+        this.renewAddIdentifierControl();
         this.addSubmitListener();
     }
 
@@ -29,28 +36,196 @@ class HistoryForm {
             buttonClass: 'btn',
             clearButton: 'true',
         })
+        this.dateRangePicker.setDates([Date.now(), null]);
     }
 
     addSubmitListener() {
         this.formElement.addEventListener("submit", this.search_Submit.bind(this));
     }
 
-    search_Submit(event){
+    renewAddIdentifierControl() {
+        if (this.addIdentifierControlElement) {
+            this.addIdentifierControlElement.remove();
+        }
+        this.addIdentifierControlElement = stringToHTML(ejs.render(add_identifier_control, { identifiers: this.remainingIdentifiers() }));
+        this.identifierFieldsContainer.appendChild(this.addIdentifierControlElement);
+        this.addIdentifierButton = this.formElement.querySelector("#add-identifier-control-button");
+        this.addIdentifierInputField = this.formElement.querySelector("#add-identifier-control-input");
+        this.addIdentifierSelectField = this.formElement.querySelector("#add-identifier-control-select");
+        this.addIdentifierInputField.addEventListener("input", this.updateAddIdentifierControlState.bind(this));
+        this.addIdentifierSelectField.addEventListener("change", this.updateAddIdentifierControlState.bind(this));
+        const self = this;
+        this.addIdentifierControlElement.addEventListener('keypress', function (event) {
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                self.handleAddIdentifierAction();
+            }
+        });
+        this.addIdentifierButton.addEventListener("click", this.handleAddIdentifierAction.bind(this));
+        this.updateAddIdentifierControlState()
+    }
+
+    updateAddIdentifierControlState = () => {
+        const addIdentifierControlContent = this.getIdentifierFieldContent(this.addIdentifierControlElement, true);
+        if (addIdentifierControlContent.identifierType) {
+            this.addIdentifierInputField.removeAttribute("disabled");
+            this.addIdentifierInputField.placeholder = this.env.identifiers[addIdentifierControlContent.identifierType].placeholder;
+        } else {
+            this.addIdentifierInputField.setAttribute("disabled", true);
+            this.addIdentifierInputField.placeholder = "";
+        }
+        if (addIdentifierControlContent.identifierType && addIdentifierControlContent.identifierValue) {
+            if (this.checkIdentifierFormat(this.addIdentifierControlElement)) {
+                this.addIdentifierButton.removeAttribute("disabled");
+                this.addIdentifierInputField.classList.remove("is-invalid");
+            } else {
+                this.addIdentifierButton.setAttribute("disabled", true);
+                this.addIdentifierInputField.classList.add("is-invalid");
+            }
+        } else {
+            this.addIdentifierButton.setAttribute("disabled", true);
+        }
+    }
+
+    handleRemoveIdentifierButtonClick(event) {
+        const identifierFieldElement = event.target.closest(".identifier-field-container");
+        identifierFieldElement.remove();
+        this.updateAddIdentifierControlState();
+        this.renewAddIdentifierControl();
+    }
+
+    addIdentifierField(content) {
+        content = { ...content, identifierLabel: this.env.identifiers[content.identifierType].label };
+        this.identifierFieldElement = stringToHTML(ejs.render(identifier_field, content));
+        this.identifierFieldsContainer.insertBefore(this.identifierFieldElement, this.addIdentifierControlElement);
+        const removeIdentifierButton = this.identifierFieldElement.querySelector(".btn-remove-identifier");
+        removeIdentifierButton.addEventListener("click", this.handleRemoveIdentifierButtonClick.bind(this));
+        const editIdentifierButton = this.identifierFieldElement.querySelector(".btn-edit-identifier");
+        editIdentifierButton.addEventListener("click", this.handleEditIdentifierButtonClick.bind(this));
+        this.identifierFieldElement.dataset.validData = true;
+        this.renewAddIdentifierControl();
+    }
+
+    handleEditIdentifierButtonClick(event) {
+        const identifierFieldElement = event.target.closest(".identifier-field-container");
+        const editIdentifierButton = identifierFieldElement.querySelector(".btn-edit-identifier");
+        const validateIdentifierButton = identifierFieldElement.querySelector(".btn-validate-identifier");
+        const inputField = identifierFieldElement.querySelector("input");
+        editIdentifierButton.classList.add("d-none");
+        validateIdentifierButton.classList.remove("d-none");
+        const self = this;
+        inputField.addEventListener('keyup', function () {
+            if (self.checkIdentifierFormat(identifierFieldElement)) {
+                inputField.classList.remove("is-invalid");
+                validateIdentifierButton.removeAttribute("disabled");
+                identifierFieldElement.dataset.validData = true;
+            } else {
+                inputField.classList.add("is-invalid");
+                validateIdentifierButton.setAttribute("disabled", true);
+                identifierFieldElement.dataset.validData = false;
+            }
+        });
+        inputField.addEventListener('keypress', function (event) {
+            if (event.key === 'Enter' && !validateIdentifierButton.hasAttribute("disabled")) {
+                event.preventDefault();
+                self.handleIdentifierValidation(event);
+            }
+        });
+        validateIdentifierButton.addEventListener("click", this.handleIdentifierValidation.bind(this));
+        inputField.removeAttribute("disabled");
+        inputField.focus();
+    }
+
+    handleIdentifierValidation(event) {
+        const identifierFieldElement = event.target.closest(".identifier-field-container");
+        const editIdentifierButton = identifierFieldElement.querySelector(".btn-edit-identifier");
+        const validateIdentifierButton = identifierFieldElement.querySelector(".btn-validate-identifier");
+        const inputField = identifierFieldElement.querySelector("input");
+        if (this.checkIdentifierFormat(identifierFieldElement)) {
+            inputField.setAttribute("disabled", true);
+            inputField.classList.remove("is-invalid");
+            editIdentifierButton.classList.remove("d-none");
+            validateIdentifierButton.classList.add("d-none");
+        } else {
+            inputField.classList.add("is-invalid");
+        }
+    }
+
+    handleAddIdentifierAction() {
+        if (this.addIdentifierButton.hasAttribute("disabled")) {
+            return;
+        }
+        this.addIdentifierField(this.getIdentifierFieldContent(this.addIdentifierControlElement, false))
+    }
+
+    getIdentifierFieldContent(newIdentifierFieldElement, explicitNullValue) {
+        const identifierType = newIdentifierFieldElement.querySelector("select.identifier-control-select").value;
+        const identifierValue = newIdentifierFieldElement.querySelector("input.identifier-control-input").value;
+        //if identifier value il an empty or blank string, return the explicit "null" value
+        // user are allowed to clear an identifier value this way
+        if (identifierValue.match(/^\s*$/)) {
+            return {
+                identifierType: identifierType,
+                identifierValue: explicitNullValue ? IDENTIFIER_NULL_VALUE : ""
+            };
+        }
+        return { identifierType: identifierType, identifierValue: identifierValue };
+    }
+
+    remainingIdentifiers() {
+        const remainingIdentifiers = {};
+        for (const identifier of Object.entries(this.env.identifiers)) {
+            if (!this.getIdentifierFieldsContent().find((identifierFieldContent) => identifierFieldContent.identifierType === identifier[0])) {
+                remainingIdentifiers[identifier[0]] = identifier[1];
+            }
+        }
+        return remainingIdentifiers;
+    }
+
+    getIdentifierFieldsContent(validOnly = false) {
+        const identifierFieldsContent = [];
+        for (const identifierFieldElement of this.formElement.querySelectorAll(".identifier-field-container")) {
+            if (validOnly && identifierFieldElement.dataset.validData === "false") {
+                continue;
+            }
+            identifierFieldsContent.push(this.getIdentifierFieldContent(identifierFieldElement, false));
+        }
+        return identifierFieldsContent;
+    }
+
+    checkIdentifierFormat(identifierFieldElement) {
+        // return true if the value is an empty or blank string to allow the user to clear the field
+        const identifierElementContent = this.getIdentifierFieldContent(identifierFieldElement, true);
+        if (identifierElementContent.identifierValue === IDENTIFIER_NULL_VALUE) {
+            return true;
+        }
+        return new RegExp(this.env.identifiers[identifierElementContent.identifierType].format).test(identifierElementContent.identifierValue);
+    }
+
+    search_Submit(event) {
         event.preventDefault();
         event.stopPropagation();
+        var textSearch
+        if (this.subpage === "publication_history") {
+            textSearch = this.formElement.querySelector("#publication-history-search-bar").value;
+        }
         const entitySubmitEvent = new CustomEvent("entity_submit",
             {
                 detail:
-                    {
-                        subpage: this.subpage,
-                        eventTypes: this.eventTypeSelect.getValue(),
-                        harvesters: this.harvestersSelect.getValue(),
-                        //TODO: Complete the base event
-                    }
+                {
+                    subpage: this.subpage,
+                    eventTypes: this.eventTypeSelect.getValue(),
+                    harvesters: this.harvestersSelect.getValue(),
+                    identifiers: this.getIdentifierFieldsContent(true),
+                    name: this.formElement.querySelector("#name-field-input").value,
+                    dateRange: this.dateRangePicker.getDates("yyyy-mm-dd"),
+                    textSearch: textSearch
+                }
             }
-            );
+        );
         this.rootElement.dispatchEvent(entitySubmitEvent)
     }
+
 }
 
 export default HistoryForm
--- app/templates/src/js/history/history_table.js
+++ app/templates/src/js/history/history_table.js
@@ -1,13 +1,30 @@
 import DataTable from 'datatables.net-dt';
+import { prettyPrintJson } from 'pretty-print-json';
+import * as bootstrap from 'bootstrap'
 
-// TODO: Complete HistoryTable, check references_table as base
-class HistoryTable{
-    constructor(env, rootElement, subpage) {
+const MAPPING_COLOR_STATE = {
+    "created": "badge-created",
+    "deleted": "badge-created",
+    "unchanged": "badge-updated",
+    "failed": "badge-failed"
+}
+
+const MAPPING_COLOR_EVENT = {
+    "created": "badge-created",
+    "deleted": "badge-deleted",
+    "updated": "badge-updated",
+}
+
+
+class HistoryTable {
+    constructor(env, rootElement, subpage, client) {
         this.env = env;
         this.rootElement = rootElement;
         this.subpage = subpage
+        this.client = client
         const config = this.getDataTableConfig();
         this.dataTable = new DataTable("#references-table", config);
+        this.addColapseListener();
     }
 
     getDataTableConfig() {
@@ -29,14 +46,15 @@ class HistoryTable{
                             data: null,
                             defaultContent: ''
                         },
-                        {"width": "15%", "title": "Date"},
-                        {"width": "15%", "title": "Nom de l'entité"},
-                        {"width": "20%", "title": "identifiants"},
-                        {"width": "15%", "title": "Type d'événements"},
-                        {"width": "15%", "title": "Sources de données"},
-                        {"width": "16%", "title": "Type de référence"},
-                        {"width": "5%", "title": "Nombre d'événements"},
-                        {"title": "Data", "visible": false}
+                        { "width": "15%", "title": "Date" },
+                        { "width": "15%", "title": "Nom de l'entité" },
+                        { "width": "20%", "title": "identifiants" },
+                        { "width": "15%", "title": "Type d'événements" },
+                        { "width": "15%", "title": "Sources de données" },
+                        { "width": "16%", "title": "Type de référence" },
+                        { "width": "5%", "title": "Nombre d'événements" },
+                        { "title": "id", "visible": false },
+                        { "title": "Data", "visible": false }
                     ]
                 };
             case "publication_history":
@@ -50,18 +68,189 @@ class HistoryTable{
                             data: null,
                             defaultContent: ''
                         },
-                        {"width": "20%", "title": "Date"},
-                        {"width": "15%", "title": "Source"},
-                        {"width": "30%", "title": "identifiant"},
-                        {"width": "15%", "title": "Statut"},
-                        {"width": "20%", "title": "Titre"},
-                        {"title": "Data", "visible": false}
+                        { "width": "20%", "title": "Date" },
+                        { "width": "15%", "title": "Source" },
+                        { "width": "30%", "title": "Identifiant" },
+                        { "width": "15%", "title": "Statut" },
+                        { "width": "20%", "title": "Titre" },
+                        { "title": "id", "visible": false },
+                        { "title": "Data", "visible": false }
                     ]
                 };
             default:
                 throw new Error(`Unsupported subpage: ${this.subpage}`);
         }
     }
+
+    addColapseListener() {
+        this.dataTable.on("click", 'td.dt-control', this.handleCollapse.bind(this));
+    }
+
+    async handleCollapse(event) {
+        if (event.target.classList.contains("dt-control")) {
+            const tr = event.target.closest("tr");
+            const row = this.dataTable.row(tr);
+            if (row.child.isShown()) {
+                row.child.hide();
+                tr.classList.remove('shown');
+            } else {
+                switch (this.subpage) {
+                    case "collection_history":
+                        const retrieval = await this.client.getRetrieval(row.data()[8]);
+                        row.data()[9] = "<pre>" + prettyPrintJson.toHtml(retrieval.data) + "</pre>";
+                        row.child(row.data()[9]).show();
+                        break;
+                    case "publication_history":
+                        const reference = await this.client.getReference(row.data()[6]);
+                        row.data()[7] = "<pre>" + prettyPrintJson.toHtml(reference.data) + "</pre>";
+                        row.child(row.data()[7]).show();
+                        break;
+                    default:
+                        throw new Error(`Unsupported subpage: ${this.subpage}`);
+                }
+                row.draw(false);
+                tr.classList.add('shown');
+            }
+        }
+
+    }
+
+    updateTable(history) {
+        switch (this.subpage) {
+            case "collection_history":
+                this.updateCollectionHistoryTable(history);
+                break;
+            case "publication_history":
+                this.updatePublicationHistoryTable(history);
+                break;
+        }
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    }
+
+    updatePublicationHistoryTable(history) {
+        const data = [];
+        for (const reference of history) {
+            const row = [
+                "",
+                reference.timestamp,
+                reference.harvester,
+                reference.source_identifier,
+                reference.event_type,
+                reference.titles,
+                reference.id,
+                ""
+            ];
+            data.push(row);
+        }
+        this.dataTable.clear();
+        this.dataTable.rows.add(data);
+        this.dataTable.draw();
+    }
+
+    updateCollectionHistoryTable(history) {
+        const data = [];
+        for (const retrieval of history) {
+            const row = [
+                "",
+                retrieval.timestamp,
+                retrieval.entity_name,
+                formatIdentifiers(retrieval.identifier_type),
+                extractUniqueActions(retrieval.harvesting_state),
+                formatSourceData(retrieval.harvesting_state),
+                formatDocumentType(retrieval.document_type),
+                retrieval.event_count,
+                retrieval.id,
+                ""
+            ];
+            data.push(row);
+        }
+        this.dataTable.clear();
+        this.dataTable.rows.add(data);
+        this.dataTable.draw();
+        const popoverTriggerList = this.rootElement.querySelectorAll('[data-bs-toggle="popover"]')
+        const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+    }
+
+}
+
+function formatSourceData(harvesting_state) {
+
+    const sources = {};
+    harvesting_state.forEach(harvesting => {
+        const source = harvesting[0];
+        if (!sources[source]) {
+            sources[source] = {
+                state: harvesting[1],
+                event_type: new Set().add([harvesting[2], harvesting[3]]),
+                count: harvesting[3]
+            }
+        } else {
+            sources[source].event_type.add([harvesting[2], harvesting[3]]);
+            sources[source].count += harvesting[3];
+        }
+    });
+    // For each source, return a bootstrap badge with the source name and the count of events
+    return Object.entries(sources).map(([source, data]) => {
+        var classBadgeState;
+        if (data.state === "failed") {
+            classBadgeState = MAPPING_COLOR_STATE[data.state];
+        } else {
+            classBadgeState = MAPPING_COLOR_STATE["unchanged"];
+            data.event_type.forEach(event => {
+                if (event[0] !== "unchanged") {
+                    classBadgeState = MAPPING_COLOR_STATE["created"];
+                }
+            });
+        }
+
+        const eventBadges = Array.from(data.event_type).map(event => {
+            return `<span class='badge ${MAPPING_COLOR_EVENT[event[0]]}'>${event[1]}</span>`;
+        }).join(' ');
+
+        return `<span class="badge badge-pill ${classBadgeState}" 
+                data-bs-toggle="tooltip"
+                data-bs-html="true" 
+                data-bs-title="${eventBadges}">
+                    ${source} ${data.count}
+                </span>`;
+
+    }).join(' ');
+}
+
+function formatIdentifiers(identifiers) {
+
+    if (!Array.isArray(identifiers) || identifiers.length === 0) {
+        return "Invalid input";
+    }
+    const formattedIdentifiers = identifiers.map(identifierPair => {
+        const [type, value] = identifierPair;
+        return `${type}(${value})`;
+    });
+
+    return formattedIdentifiers.join(' OR ');
+}
+
+function extractUniqueActions(data) {
+    if (!Array.isArray(data) || data.length === 0) {
+        return "Invalid input";
+    }
+
+    const uniqueActions = new Set(data.map(item => item[2]));
+
+    return Array.from(uniqueActions).join(', ');
+}
+
+function formatDocumentType(documentType) {
+    if (!Array.isArray(documentType) || documentType.length === 0) {
+        return "Invalid input";
+    }
+
+    const filteredTypes = documentType.filter(type => type !== null);
+    const result = filteredTypes.join(', ');
+
+    return result;
 }
 
 export default HistoryTable
