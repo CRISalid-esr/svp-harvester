4334ca62946ff994d093be2844378b10eef13bdd
E-Bara <62282795+E-Bara@users.noreply.github.com>
2024-02-07 15:21:02 +0100

    Unknown HAL document type: HDR

--- app/harvesters/hal/hal_document_type_converter.py
+++ app/harvesters/hal/hal_document_type_converter.py
@@ -32,7 +32,7 @@ class HalDocumentTypeConverter(AbstractDocumentTypeConverter):
         "ISSUE": (f"{RDF_BIBO}Document", "Document"),
         "NOTICE": (f"{RDF_BIBO}Document", "Document"),
         "PROCEEDINGS": (f"{RDF_BIBO}Proceedings", "Proceedings"),
-        "HRD": (f"{RDF_BIBO}Document", "Document"),
+        "HDR": (f"{RDF_BIBO}Document", "Document"),
         "BLOG": (f"{RDF_COAR}c_6947", "Blog Post"),
         "PATENT": (f"{RDF_BIBO}Patent", "Patent"),
         "REPORT_LABO": (f"{RDF_COAR}c_93fc", "Report"),
