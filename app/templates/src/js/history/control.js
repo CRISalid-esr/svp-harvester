efc186deb2ea4dda8e9658364e89ae9fa315a0f1
Pablo Aldana <pablo.aldana@uc.cl>
2024-02-07 06:33:43 +0100

    265 history page retrieve data (#271)
    * feat: api retrieval history
    
    * feat: get state of retrieval
    
    * feat: harvesting count
    
    * feat: get reference summary
    
    * refacto: retrieval endpoint
    
    * fix: reference title None
    
    * feat: search by text
    
    * chore: add qs
    
    * feat: history pages (collections, publications)
    
    * feat: harvester filter
    
    * feat: history data sources
    
    * refacto: add fetch_summary function
    
    * refacto: DAO reference and retrieval
    
    * feat: Disable button to submit history
    
    ---------
    
    Co-authored-by: Paldana99 <pablo.a@galadrim.fr>

--- app/templates/src/js/history/control.js
+++ app/templates/src/js/history/control.js
@@ -1,11 +1,64 @@
 // TODO: Complete Control for History page. Refer to Control from retrieve as base
-class Control{
-    constructor(env, form, historyTable, rootElement, client) {
+class Control {
+    constructor(env, form, historyTable, rootElement, subPage, client) {
         this.env = env;
         this.form = form;
         this.historyTable = historyTable;
         this.rootElement = rootElement;
+        this.subPage = subPage
         this.client = client
+        this.addSubmitListener();
+    }
+
+    addSubmitListener() {
+        this.rootElement.addEventListener("entity_submit", this.handleSubmit.bind(this));
+    }
+
+    handleSubmit(event) {
+        const formIdentifiers = event.detail.identifiers;
+        const formName = event.detail.name === "" ? null : event.detail.name;
+        const eventTypes = event.detail.eventTypes;
+        const harvesters = event.detail.harvesters;
+        const dateRange = event.detail.dateRange;
+        const identifiers = {}
+        formIdentifiers.forEach(identifier => {
+            identifiers[identifier.identifierType] = identifier.identifierValue
+
+        })
+
+        let params = {
+            ...identifiers,
+            name: formName,
+            events: eventTypes,
+            harvester: harvesters,
+            date_start: dateRange[0],
+            date_end: dateRange[1],
+        }
+        switch (this.subPage) {
+            case "collection_history":
+                this.client.getHistoryRetrieval(params).then((response) => {
+                    const retrieval = response.data
+                    this.historyTable.updateTable(retrieval);
+                }).catch((error) => {
+                    console.log(error);
+                });
+                break;
+            case "publication_history":
+                params = {
+                    ...params,
+                    text_search: event.detail.textSearch
+                }
+                this.client.getHistoryPublication(params).then((response) => {
+                    const retrieval = response.data
+                    this.historyTable.updateTable(retrieval);
+                }).catch((error) => {
+                    console.log(error);
+                });
+                break;
+            default:
+                throw new Error(`Unsupported subpage: ${this.subPage}`);
+
+        }
     }
 
 }
