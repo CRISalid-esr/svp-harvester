f110e4ae2ad01b51b61334c288626b35cd19e40f
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-02-15 18:04:33 +0100

    Fix erroneous query

--- app/amqp/amqp_reference_event_message_factory.py
+++ app/amqp/amqp_reference_event_message_factory.py
@@ -26,12 +26,10 @@ class AMQPReferenceEventMessageFactory(AbstractAMQPMessageFactory):
     async def _build_payload(self) -> dict[str, Any]:
         async with async_session() as session:
             async with session.begin():
-                result = await ReferenceEventDAO(
+                reference_event: DbReferenceEvent = await ReferenceEventDAO(
                     session
                 ).get_detailed_reference_event_by_id(self.content.get("id"))
-                reference_event: DbReferenceEvent = result[0]
-                self.reference_event_type = reference_event.type
-                entity: DbEntity = result[1]
+                entity: DbEntity = reference_event.harvesting.retrieval.entity
                 reference_event_representation: ReferenceEventModel = (
                     ReferenceEventModel.model_validate(reference_event)
                 )
