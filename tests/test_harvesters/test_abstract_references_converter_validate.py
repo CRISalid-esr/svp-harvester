e3b2751b9130480f44442d31bfc7f883d5aae83e
E-Bara <62282795+E-Bara@users.noreply.github.com>
2024-02-21 12:01:31 +0100

    feat: Add test for validate_reference decorator

--- tests/test_harvesters/test_abstract_references_converter_validate.py
+++ tests/test_harvesters/test_abstract_references_converter_validate.py
@@ -3,36 +3,90 @@ Test the validate_reference decorator of the AbstractReferencesConverter class.
 """
 import pytest
 
+from app.db.models.abstract import Abstract as DbAbstract
+from app.db.models.contribution import Contribution as DbContribution
+from app.db.models.contributor import Contributor as DbContributor
 from app.db.models.reference import Reference as DbReference
+from app.db.models.subtitle import Subtitle as DbSubtitle
 from app.db.models.title import Title as DbTitle
 from app.harvesters.abstract_references_converter import AbstractReferencesConverter
 
 
 @pytest.fixture(name="reference_without_title")
 def fixture_reference_without_title():
+    """
+    Return a reference without title
+    """
     return DbReference(
         source_identifier="I am unique !",
+        harvester="test_harvester",
         titles=[],
+    )
+
+
+@pytest.fixture(name="reference_without_harvester")
+def fixture_reference_without_harvester():
+    """
+    Return a reference without harvester
+    """
+    return DbReference(
+        source_identifier="I am unique !",
+        harvester="",
+        titles=[DbTitle(value="I am a title")],
+    )
+
+
+@pytest.fixture(name="reference_with_abstract_value_none")
+def fixture_reference_with_abstract_value_none():
+    """
+    Return a reference without harvester
+    """
+    return DbReference(
+        source_identifier="I am unique !",
+        harvester="test",
+        titles=[DbTitle(value="I am a title")],
+        abstracts=[],
     )
 
 
 @pytest.fixture(name="reference_without_identifier")
 def fixture_reference_without_identifier():
+    """
+    Return a reference without identifier
+    """
     return DbReference(
         titles=[DbTitle(value="I am a title")],
     )
 
 
-@pytest.fixture(name="reference_with_title_and_identifier")
-def fixture_reference_with_title_and_identifier():
+@pytest.fixture(name="reference_with_all_fields")
+def fixture_reference_with_all_fields():
+    """
+    Return a reference with all the fields set.
+    """
     return DbReference(
         source_identifier="I am unique !",
         titles=[DbTitle(value="I am a title")],
+        harvester="test_harvester",
+        abstracts=[DbAbstract(value="I am an abstract")],
+        subtitles=[DbSubtitle(value="I am a subtitle")],
+        document_type=["I am a document type"],
+        contributions=[
+            DbContribution(
+                rank=1,
+                role="I am a role",
+                contributor=DbContributor(
+                    source="I am a source",
+                    source_identifier="I am a source identifier",
+                    name="I am a name",
+                    name_variants=["I am a name variant"],
+                ),
+            )
+        ],
     )
 
 
 @pytest.mark.asyncio
-@pytest.mark.current
 async def test_reference_without_title_raises_exception(reference_without_title):
     """
     Given a reference without title
@@ -55,7 +109,6 @@ async def test_reference_without_title_raises_exception(reference_without_title)
 
 
 @pytest.mark.asyncio
-@pytest.mark.current
 async def test_reference_without_identifier_raises_exception(
     reference_without_identifier,
 ):
@@ -80,24 +133,73 @@ async def test_reference_without_identifier_raises_exception(
 
 
 @pytest.mark.asyncio
+async def test_reference_without_harvester_raises_exception(
+    reference_without_harvester,
+):
+    """ "
+    Given a reference without identifier
+    When the validate_reference decorator is called
+    Then an AssertionError should be raised
+
+    :param reference_without_harvester: A reference without identifier
+    """
+
+    async def decorated_function(self):
+        return reference_without_harvester
+
+    function_with_decorator = AbstractReferencesConverter.validate_reference(
+        decorated_function
+    )
+    with pytest.raises(AssertionError) as exc_info:
+        await function_with_decorator(AbstractReferencesConverter)
+
+    assert exc_info.match("harvester should be set on reference")
+
+
+@pytest.mark.asyncio
 @pytest.mark.current
-async def test_reference_with_title_and_identifier_does_not_raise_exception(
-    reference_with_title_and_identifier,
+async def test_reference_with_abstract_value_none_raises_exception(
+    reference_with_abstract_value_none,
+):
+    """
+    Given a reference without identifier
+    When the validate_reference decorator is called
+    Then an AssertionError should be raised
+
+    :param reference_with_abstract_value_none: A reference without identifier
+    """
+
+    async def decorated_function(self):
+        reference_with_abstract_value_none.abstracts = None
+        return reference_with_abstract_value_none
+
+    function_with_decorator = AbstractReferencesConverter.validate_reference(
+        decorated_function
+    )
+    with pytest.raises(AssertionError) as exc_info:
+        await function_with_decorator(AbstractReferencesConverter)
+
+    assert exc_info.match("harvester should be set on reference")
+
+
+@pytest.mark.asyncio
+async def test_reference_with_all_fields_does_not_raise_exception(
+    reference_with_all_fields,
 ):
     """
     Given a reference with title and identifier
     When the validate_reference decorator is called
     Then the reference should be returned
 
-    :param reference_with_title_and_identifier:
+    :param reference_with_all_fields:
     """
 
     async def decorated_function(self):
-        return reference_with_title_and_identifier
+        return reference_with_all_fields
 
     function_with_decorator = AbstractReferencesConverter.validate_reference(
         decorated_function
     )
     result = await function_with_decorator(AbstractReferencesConverter)
 
-    assert result == reference_with_title_and_identifier
+    assert result == reference_with_all_fields
