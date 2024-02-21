3c68e339bb303d7c5347070a24fb4a02f08a5018
E-Bara <62282795+E-Bara@users.noreply.github.com>
2024-02-21 17:37:48 +0100

    test: Add tests for scanr subjects

--- tests/fixtures/scanr_api_docs_fixtures.py
+++ tests/fixtures/scanr_api_docs_fixtures.py
@@ -47,6 +47,42 @@ def fixture_scanr_api_docs_for_authors_dupe(_base_path) -> dict:
     )
 
 
+@pytest.fixture(name="scanr_publication_doc_with_keywords_domains")
+def fixture_scanr_publication_doc_with_keywords_concepts(_base_path) -> dict:
+    """
+    Generate a SCANR API response for a person in JSON format
+    :param _base_path: test data directory base
+    :return: SCANR API response for one researcher in JSON format
+    """
+    return _scanr_api_json_data_from_file(
+        _base_path, "scanr_publication_doc_with_keywords_domains"
+    )
+
+
+@pytest.fixture(name="scanr_publication_doc_with_sudoc_domains")
+def fixture_scanr_publication_doc_with_sudoc_domains(_base_path) -> dict:
+    """
+    Generate a SCANR API response for a person in JSON format
+    :param _base_path: test data directory base
+    :return: SCANR API response for one researcher in JSON format
+    """
+    return _scanr_api_json_data_from_file(
+        _base_path, "scanr_publication_doc_with_sudoc_domains"
+    )
+
+
+@pytest.fixture(name="scanr_publication_doc_with_wikidata_domains")
+def fixture_scanr_publication_doc_with_wikidata_domains(_base_path) -> dict:
+    """
+    Generate a SCANR API response for a person in JSON format
+    :param _base_path: test data directory base
+    :return: SCANR API response for one researcher in JSON format
+    """
+    return _scanr_api_json_data_from_file(
+        _base_path, "scanr_publication_doc_with_wikidata_domains"
+    )
+
+
 def _scanr_api_json_data_from_file(base_path, file_name) -> dict:
     file_path = f"data/scanr_api/{file_name}.json"
     return _json_data_from_file(base_path, file_path)
---
+++ tests/test_harvesters/test_scanr/test_scanr_references_converter_for_concepts.py
@@ -0,0 +1,100 @@
+import pytest
+
+from app.harvesters.json_harvester_raw_result import JsonHarvesterRawResult
+from app.harvesters.scanr.scanr_references_converter import ScanrReferencesConverter
+
+
+@pytest.fixture(name="scanr_publication_doc_with_keywords_domains")
+def fixture_scanr_api_publication_cleaned_response(
+    scanr_publication_doc_with_keywords_domains,
+):
+    """Return the list of dictionaries references from scanr response"""
+    return scanr_publication_doc_with_keywords_domains["hits"]["hits"]
+
+
+@pytest.fixture(name="scanr_publication_doc_with_sudoc_domains")
+def fixture_scanr_publication_doc_with_sudoc_domains(
+    scanr_publication_doc_with_sudoc_domains,
+):
+    """Return the list of dictionaries references from scanr response"""
+    return scanr_publication_doc_with_sudoc_domains["hits"]["hits"]
+
+
+@pytest.fixture(name="scanr_publication_doc_with_wikidata_domains")
+def fixture_scanr_publication_doc_with_wikidata_domains(
+    scanr_publication_doc_with_wikidata_domains,
+):
+    """Return the list of dictionaries references from scanr response"""
+    return scanr_publication_doc_with_wikidata_domains["hits"]["hits"]
+
+
+@pytest.mark.current
+async def test_convert_publication_with_keywords(
+    scanr_publication_doc_with_keywords_domains,
+):
+    converter_under_tests = ScanrReferencesConverter()
+
+    expected_subjects = [(None, "Efficacité énergétique")]
+
+    for doc in scanr_publication_doc_with_keywords_domains:
+        result = JsonHarvesterRawResult(
+            source_identifier=doc.get("_id"), payload=doc, formatter_name="SCANR"
+        )
+
+        test_reference = await converter_under_tests.convert(result)
+
+        test_subjects = [
+            (label.language, label.value)
+            for concept in test_reference.subjects
+            for label in concept.labels
+        ]
+        assert test_subjects == expected_subjects
+
+
+@pytest.mark.current
+async def test_convert_publication_with_sudoc(
+    scanr_publication_doc_with_sudoc_domains,
+):
+    converter_under_tests = ScanrReferencesConverter()
+
+    expected_subjects = [(None, "Internet des objets")]
+
+    for doc in scanr_publication_doc_with_sudoc_domains:
+        result = JsonHarvesterRawResult(
+            source_identifier=doc.get("_id"), payload=doc, formatter_name="SCANR"
+        )
+
+        test_reference = await converter_under_tests.convert(result)
+
+        test_subjects = [
+            (label.language, label.value)
+            for concept in test_reference.subjects
+            for label in concept.labels
+        ]
+        assert test_subjects == expected_subjects
+
+
+@pytest.mark.current
+async def test_convert_publication_with_wikidata(
+    scanr_publication_doc_with_wikidata_domains,
+):
+    converter_under_tests = ScanrReferencesConverter()
+
+    expected_subjects = [(None, "Efficacité énergétique")]
+
+    for doc in scanr_publication_doc_with_wikidata_domains:
+        result = JsonHarvesterRawResult(
+            source_identifier=doc.get("_id"), payload=doc, formatter_name="SCANR"
+        )
+
+        test_reference = await converter_under_tests.convert(result)
+
+        test_subjects = [
+            (label.language, label.value)
+            for concept in test_reference.subjects
+            for label in concept.labels
+        ]
+        assert test_subjects == expected_subjects
+
+
+# TODO: Make tests who tell the expected results of the new convert method
