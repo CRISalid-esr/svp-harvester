2cbcafbe74516af2669e096473daa3f2dd18659c
Pablo Aldana <pablo.aldana@uc.cl>
2024-03-01 15:34:51 +0100

    feat: Add partial search in history name entity (#366)
    Co-authored-by: Paldana99 <pablo.a@galadrim.fr>

--- app/utilities/string_utilities.py
+++ app/utilities/string_utilities.py
@@ -1,3 +1,5 @@
+import re
+
 
 def remove_after_separator(value: str, separator: str):
     """
@@ -5,3 +7,15 @@ def remove_after_separator(value: str, separator: str):
     """
     new_value = value.partition(separator)[0].strip()
     return new_value
+
+
+def split_string(s: str):
+    """
+    Split a string by spaces and commas into a list of strings,
+    removing multiple spaces.
+    """
+    list_string = s.replace(",", " ").strip()
+    # remove multiple spaces
+    list_string = re.sub(" +", " ", list_string)
+    list_string = list_string.split(" ")
+    return list_string
