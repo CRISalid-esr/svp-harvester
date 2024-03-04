651855a17e45547084b6bd845fd3ec9aba9d907b
E-Bara <62282795+E-Bara@users.noreply.github.com>
2024-03-04 09:37:03 +0100

    Feat: Add Curator Quality

--- app/db/models/contribution.py
+++ app/db/models/contribution.py
@@ -24,6 +24,7 @@ class Contribution(Base):
         ANALYST = "Analyst"
         CORRESPONDENT_AUTHOR = "Correspondent Author"
         CONTRIBUTOR = "Contributor"
+        CURATOR = "Curator"
         ANNOTATOR = "Annotator"
         COMPILER = "Compiler"
         COMPOSER = "Composer"
