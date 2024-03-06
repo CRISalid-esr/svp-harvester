f8df5c173b1bdf340892aad600323c9043110991
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-03-06 12:30:42 +0100

    Journal/issue models with tests

--- app/db/models/reference.py
+++ app/db/models/reference.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import List
 
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.models.abstract import Abstract
@@ -69,30 +70,37 @@ class Reference(Base, VersionedRecord):
         lazy="noload",
     )
 
-    document_type: Mapped[List["app.db.models.document_type.DocumentType"]] = (
-        relationship(
-            "app.db.models.document_type.DocumentType",
-            secondary=references_document_type_table,
-            lazy="joined",
-        )
+    document_type: Mapped[
+        List["app.db.models.document_type.DocumentType"]
+    ] = relationship(
+        "app.db.models.document_type.DocumentType",
+        secondary=references_document_type_table,
+        lazy="joined",
     )
 
-    reference_events: Mapped[List["app.db.models.reference_event.ReferenceEvent"]] = (
-        relationship(
-            "app.db.models.reference_event.ReferenceEvent",
-            back_populates="reference",
-            cascade="all, delete",
-            lazy="raise",
-        )
+    reference_events: Mapped[
+        List["app.db.models.reference_event.ReferenceEvent"]
+    ] = relationship(
+        "app.db.models.reference_event.ReferenceEvent",
+        back_populates="reference",
+        cascade="all, delete",
+        lazy="raise",
     )
 
-    contributions: Mapped[List["app.db.models.contribution.Contribution"]] = (
-        relationship(
-            "app.db.models.contribution.Contribution",
-            back_populates="reference",
-            cascade="all, delete",
-            lazy="joined",
-        )
+    contributions: Mapped[
+        List["app.db.models.contribution.Contribution"]
+    ] = relationship(
+        "app.db.models.contribution.Contribution",
+        back_populates="reference",
+        cascade="all, delete",
+        lazy="joined",
+    )
+
+    issue_id: Mapped[int] = mapped_column(ForeignKey("issues.id"), nullable=True)
+    issue: Mapped["app.db.models.issue.Issue"] = relationship(
+        "app.db.models.issue.Issue",
+        back_populates="references",
+        lazy="raise",
     )
 
     issued: Mapped[datetime] = mapped_column(nullable=True, index=True)
