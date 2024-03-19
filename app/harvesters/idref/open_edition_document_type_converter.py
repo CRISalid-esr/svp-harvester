df6887e3d357733585120a0349dfdfe9d3038054
E-Bara <62282795+E-Bara@users.noreply.github.com>
2024-03-19 13:36:43 +0100

    Refactor: refacto document_type_converters

--- app/harvesters/idref/open_edition_document_type_converter.py
+++ app/harvesters/idref/open_edition_document_type_converter.py
@@ -1,34 +1,21 @@
-from loguru import logger
+from app.harvesters.abstract_document_type_converter import (
+    AbstractDocumentTypeConverter,
+)
 
 
-class OpenEditionDocumentTypeConverter:
+class OpenEditionDocumentTypeConverter(AbstractDocumentTypeConverter):
     """
     Use mapping table to convert Open Edition document type to loc document type values
     """
 
-    RDF_BIBO = "http://purl.org/ontology/bibo/"
-    RDF_SPAR = "http://purl.org/spar/fabio/"
-    RDF_COAR = "http://purl.org/coar/resource_type"
+    HARVESTER = "Open_Edition"
 
-    CODES_MAPPING = {
-        "article": (f"{RDF_BIBO}Article", "Article"),
-        "introduction": (f"{RDF_BIBO}Chapter", "Chapter"),
-        "review": (f"{RDF_BIBO}Article", "Article"),
-        "archaelogical note": (f"{RDF_BIBO}Note", "Note"),
-        "chapter": (f"{RDF_BIBO}Chapter", "Chapter"),
+    RDF = AbstractDocumentTypeConverter.RDF
+
+    TYPES_MAPPING = {
+        "article": (f"{RDF['BIBO']}Article", "Article"),
+        "introduction": (f"{RDF['BIBO']}Chapter", "Chapter"),
+        "review": (f"{RDF['BIBO']}Article", "Article"),
+        "archaelogical note": (f"{RDF['BIBO']}Note", "Note"),
+        "chapter": (f"{RDF['BIBO']}Chapter", "Chapter"),
     }
-
-    UKNOWN_CODE = ("http://data.crisalid.org/ref/document_types/unkown", "Uknown")
-
-    @staticmethod
-    def convert(type_document: str) -> tuple[str, str]:
-        """
-        Given an Open Edition document type, return the corresponding loc document type
-        """
-        if type_document not in OpenEditionDocumentTypeConverter.CODES_MAPPING:
-            logger.warning(
-                f"Unknown Open Edition document type: {type_document}",
-            )
-            return OpenEditionDocumentTypeConverter.UKNOWN_CODE
-
-        return OpenEditionDocumentTypeConverter.CODES_MAPPING[type_document]
