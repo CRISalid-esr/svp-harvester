7d6436c0b89375a5bd57dd1acd239f17c3c10464
Pablo Aldana <pablo.aldana@uc.cl>
2024-03-21 13:35:54 +0100

    437 open alex journal issue (#438)
    * feat: Update model issue
    
    * feat: Add journal and issue DAO
    
    * feat: Convert Hal issue/journal
    
    * test: Add issue journal test to openalex
    
    * feat: Handle array issn, eissn. Add issn_l
    
    ---------
    
    Co-authored-by: Paldana99 <pablo.a@galadrim.fr>

--- tests/test_db/test_daos/test_journal_dao.py
+++ tests/test_db/test_daos/test_journal_dao.py
@@ -13,23 +13,32 @@ async def test_get_journal_by_source_issn_or_eissn(async_session):
     """
     journal = Journal(
         source="hal",
-        issn="1234-5678",
-        eissn="9876-5432",
+        issn=["1234-5678"],
+        eissn=["9876-5432"],
+        issn_l="1234-5678",
         source_identifier="hal-1234-5678",
     )
     async_session.add(journal)
     await async_session.commit()
 
     dao = JournalDAO(async_session)
-    journal_from_db = await dao.get_journal_by_source_issn_or_eissn("hal", "1234-5678")
-    assert isinstance(journal_from_db, Journal)
-    assert journal_from_db.issn == "1234-5678"
-
-    journal_from_db = await dao.get_journal_by_source_issn_or_eissn(
-        "hal", eissn="9876-5432"
+    journal_from_db = await dao.get_journal_by_source_issn_or_eissn_or_issn_l(
+        "hal", issn=["1234-5678"]
     )
     assert isinstance(journal_from_db, Journal)
-    assert journal_from_db.issn == "1234-5678"
+    assert "1234-5678" in journal_from_db.issn
+
+    journal_from_db = await dao.get_journal_by_source_issn_or_eissn_or_issn_l(
+        "hal", eissn=["9876-5432"]
+    )
+    assert isinstance(journal_from_db, Journal)
+    assert "1234-5678" in journal_from_db.issn
+
+    journal_from_db = await dao.get_journal_by_source_issn_or_eissn_or_issn_l(
+        "hal", issn_l="1234-5678"
+    )
+    assert isinstance(journal_from_db, Journal)
+    assert "1234-5678" in journal_from_db.issn
 
 
 @pytest.mark.asyncio
@@ -48,7 +57,7 @@ async def test_get_without_issn_and_eissn(async_session):
     await async_session.commit()
 
     dao = JournalDAO(async_session)
-    journal_from_db = await dao.get_journal_by_source_issn_or_eissn("hal")
+    journal_from_db = await dao.get_journal_by_source_issn_or_eissn_or_issn_l("hal")
     assert journal_from_db is None
 
 
