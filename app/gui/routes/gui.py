e7e6f2cf72d9f9c00a36109c6e6fee12e5fa3543
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2024-03-30 15:44:19 +0100

    Add redirection from root (#484)

--- app/gui/routes/gui.py
+++ app/gui/routes/gui.py
@@ -2,8 +2,14 @@
 API redirection router
 """
 from fastapi import APIRouter
+from starlette.responses import RedirectResponse
 
 from app.gui.routes import admin
 
 router = APIRouter()
 router.include_router(admin.router, tags=["admin"], prefix="/admin")
+
+
+@router.get("/")
+def redirect_to_admin():
+    return RedirectResponse(url="/admin")
