6886f9737d544ed981b2934f2e091a710b9dc480
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-04-06 11:42:03 +0200

    Add static display

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/js/main.js',
@@ -43,5 +44,12 @@ module.exports = {
                 }]
             }
         ]
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: 'images', to: path.resolve(__dirname, '../static/images')}
+            ]
+        })
+    ]
 }
