0433212eb14770e3439f2a74478e004635750ef2
E-Bara <62282795+E-Bara@users.noreply.github.com>
2024-04-23 15:01:26 +0200

    Refacto: Idref take account of LOCAuthorRoles without mapping

--- app/harvesters/idref/idref_qualities_converter.py
+++ app/harvesters/idref/idref_qualities_converter.py
@@ -2,20 +2,11 @@ from loguru import logger
 
 from app.db.models.contribution import Contribution
 
-# TODO: Complete the IdrefQualitiesConverter class to use LOCAuthorRoles enum by default,
-#  without needing mapping table
-
 
 class IdrefQualitiesConverter:
     """
     Use mapping table to convert idref role values to loc roles values
     """
-
-    ROLES_MAPPING = {
-        "aut": Contribution.LOCAuthorRoles.AUT,
-        "cur": Contribution.LOCAuthorRoles.CUR,
-        "ths": Contribution.LOCAuthorRoles.THS,
-    }
 
     @staticmethod
     def convert(quality: str) -> str:
@@ -24,7 +15,10 @@ class IdrefQualitiesConverter:
         :param quality: science plus role value
         :return: loc role value
         """
-        if quality not in IdrefQualitiesConverter.ROLES_MAPPING:
-            logger.warning(f"Unknown idref quality: {quality}")
-            return Contribution.LOCAuthorRoles.UNKNOWN.loc_url()
-        return IdrefQualitiesConverter.ROLES_MAPPING[quality].loc_url()
+        quality_upper = quality.upper()
+
+        if hasattr(Contribution.LOCAuthorRoles, quality_upper):
+            return getattr(Contribution.LOCAuthorRoles, quality_upper).loc_url()
+
+        logger.warning(f"Unknown idref quality: {quality}")
+        return Contribution.LOCAuthorRoles.UNKNOWN.loc_url()
