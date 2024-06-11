0bc0a6f621f1bac3e1709597cd88efeb4f2a4068
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-06-11 16:21:12 +0200

    Complete tests

--- tests/test_models/test_reference_manifestations.py
+++ tests/test_models/test_reference_manifestations.py
@@ -4,6 +4,7 @@ import pytest
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.db.models.reference import Reference
 from app.db.models.reference_manifestation import ReferenceManifestation
 
 
@@ -33,26 +34,32 @@ def test_manifestation_with_valid_url():
     assert manifestation.download_url == "http://example.com"
 
 
-async def test_manifestation_without_page_fails(async_session: AsyncSession):
+async def test_manifestation_without_page_fails(
+    async_session: AsyncSession, reference_db_model: Reference
+):
     """
     GIVEN a manifestation without a page
     WHEN the manifestation is created
     THEN check a validation error is raised
     """
     manifestation = ReferenceManifestation(download_url="http://example.com")
-    async_session.add(manifestation)
+    reference_db_model.manifestations.append(manifestation)
+    async_session.add(reference_db_model)
     with pytest.raises(IntegrityError):
         await async_session.commit()
 
 
-async def test_manifestation_without_download_url_succeeds(async_session: AsyncSession):
+async def test_manifestation_without_download_url_succeeds(
+    async_session: AsyncSession, reference_db_model: Reference
+):
     """
     GIVEN a manifestation without a download URL
     WHEN the manifestation is created
     THEN check the manifestation is created
     """
     manifestation = ReferenceManifestation(page="http://example.com")
-    async_session.add(manifestation)
+    reference_db_model.manifestations.append(manifestation)
+    async_session.add(reference_db_model)
     await async_session.commit()
     await async_session.refresh(manifestation)
     assert manifestation.page == "http://example.com"
