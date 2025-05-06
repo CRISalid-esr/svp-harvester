288de63dabf028d52cb15efe127ee520190e9cf7
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-05-06 07:44:49 +0200

    Support old Hal domain name

--- app/services/identifiers/rules/hal_id_from_hal_url.py
+++ app/services/identifiers/rules/hal_id_from_hal_url.py
@@ -20,21 +20,23 @@ class HalIdFromHalUrlRule(IdentifierInferenceRule):
 
     @staticmethod
     def _load_patterns():
-        yml_file = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                           "./hal_accepted_patterns.yaml")
+        yml_file = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "./hal_accepted_patterns.yaml"
+        )
         try:
-            with open(yml_file, 'r', encoding='UTF-8') as file:
+            with open(yml_file, "r", encoding="UTF-8") as file:
                 data = yaml.safe_load(file)
 
-            if 'accepted_patterns' not in data:
-                logger.warning(f"The 'accepted_patterns' key is not present in {yml_file}")
+            if "accepted_patterns" not in data:
+                logger.warning(
+                    f"The 'accepted_patterns' key is not present in {yml_file}"
+                )
 
-            return data['accepted_patterns']
+            return data["accepted_patterns"]
 
         except FileNotFoundError as error:
             logger.error(error)
             return []
-
 
     def infer(self, reference: Reference) -> None:
         """
@@ -58,13 +60,14 @@ class HalIdFromHalUrlRule(IdentifierInferenceRule):
         parsed_url = urlparse(url)
         if not all([parsed_url.scheme, parsed_url.netloc]):
             return None
-        if "hal." not in parsed_url.netloc:
+        hal_domain_keywords = ["hal.", "archives-ouvertes.fr"]
+        if not any(kw in parsed_url.netloc for kw in hal_domain_keywords):
             return None
         url_path = parsed_url.path.rstrip("/document")
         # e.g. 'https://shs.hal.science/halshs-02185511/file/Science%20paper%20ORI.pdf'
         url_path = re.sub(r"/file(/.*)?$", "", url_path)
         hal_id = url_path.split("/")[-1]
-        match = re.match(fr"({'|'.join(self.accepted_patterns)})-\d+(v\d+)?", hal_id)
+        match = re.match(rf"({'|'.join(self.accepted_patterns)})-\d+(v\d+)?", hal_id)
         if not match:
             logger.warning(f"Unknown HAL identifier pattern: {hal_id}")
             return None
