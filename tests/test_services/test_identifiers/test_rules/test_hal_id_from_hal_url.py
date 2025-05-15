d1ebf2405b7eae89156a402eda42982168da086a
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2025-05-15 08:22:29 +0200

    Support old Hal domain name (#692)
    * Support old Hal domain name
    
    * Increase harvester version number

--- tests/test_services/test_identifiers/test_rules/test_hal_id_from_hal_url.py
+++ tests/test_services/test_identifiers/test_rules/test_hal_id_from_hal_url.py
@@ -2,24 +2,38 @@ import pytest
 
 from app.services.identifiers.rules.hal_id_from_hal_url import HalIdFromHalUrlRule
 
+import pytest
 
 
+@pytest.mark.current
 @pytest.mark.parametrize(
     "url_to_test, expected_value",
     [
-        ("https://shs.hal.science/halshs-02185511/file/Science%20paper%20ORI.pdf",
-         "halshs-02185511"),
-        ("https://shs.hal.science/halshs-02185511v4/file/Science%20paper%20ORI.pdf",
-         "halshs-02185511"),
-        ("https://shs.hal.science/jpa-02185511/file/Science%20paper%20ORI.pdf",
-         "jpa-02185511"),
-        ("https://shs.hal.science/invalid-02185511/file/Science%20paper%20ORI.pdf",
-         None),
-        ("https://shs.invalid.science/halshs-02185511/file/Science%20paper%20ORI.pdf",
-         None),
-        ("shs.hal.science/jpa-02185511/file/Science%20paper%20ORI.pdf",
-         None),
-        (None, None)
+        (
+            "https://shs.hal.science/halshs-02185511/file/Science%20paper%20ORI.pdf",
+            "halshs-02185511",
+        ),
+        (
+            "https://shs.hal.science/halshs-02185511v4/file/Science%20paper%20ORI.pdf",
+            "halshs-02185511",
+        ),
+        (
+            "https://shs.hal.science/jpa-02185511/file/Science%20paper%20ORI.pdf",
+            "jpa-02185511",
+        ),
+        (
+            "https://shs.hal.science/invalid-02185511/file/Science%20paper%20ORI.pdf",
+            None,
+        ),
+        (
+            "https://shs.invalid.science/halshs-02185511/file/Science%20paper%20ORI.pdf",
+            None,
+        ),
+        ("shs.hal.science/jpa-02185511/file/Science%20paper%20ORI.pdf", None),
+        (None, None),
+        ("https://halshs.archives-ouvertes.fr/halshs-02008468", "halshs-02008468"),
+        ("https://hal.archives-ouvertes.fr/halshs-02008468", "halshs-02008468"),
+        ("https://halshs.archives-ouvertes.fr/halshs-01275331/", "halshs-01275331"),
     ],
     ids=[
         "test_extract_hal_id",
@@ -29,7 +43,10 @@ from app.services.identifiers.rules.hal_id_from_hal_url import HalIdFromHalUrlRu
         "test_extract_hal_id_with_invalid_netloc",
         "test_extract_hal_id_with_unknown_url_scheme",
         "test_extract_hal_id_without_url",
-    ]
+        "test_extract_hal_id_from_halshs_archives_ouvertes",
+        "test_extract_hal_id_from_hal_archives_ouvertes",
+        "test_extract_hal_id_from_halshs_archives_ouvertes_with_trailing_slash",
+    ],
 )
 def test_hal_id_from_hal_url(url_to_test, expected_value):
     """
@@ -45,12 +62,11 @@ def test_hal_id_from_hal_url(url_to_test, expected_value):
 
 def test_extract_hal_id_check_loguru_warning(caplog):
     """
-    Test the extract_hal_id function in HalIdFromHalUrlRule Class
-    :param url_to_test:
-    :param expected_value:
-    :return:
+    Test the extract_hal_id function logs a warning for unknown HAL patterns
     """
-    url_to_test = "https://shs.hal.science/invalid-02185511/file/Science%20paper%20ORI.pdf"
+    url_to_test = (
+        "https://shs.hal.science/invalid-02185511/file/Science%20paper%20ORI.pdf"
+    )
     class_to_test = HalIdFromHalUrlRule()
     returned_value = class_to_test.extract_hal_id(url_to_test)
     expected_warning = "Unknown HAL identifier pattern: invalid-02185511"
