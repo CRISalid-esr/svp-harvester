73a25d031d86fe340fd14ad736892b7ee5a88005
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2026-02-07 11:01:05 +0100

    Fix starlette error code

--- app/api/errors/validation_error.py
+++ app/api/errors/validation_error.py
@@ -4,7 +4,7 @@ import json
 from pydantic import ValidationError
 from starlette.requests import Request
 from starlette.responses import JSONResponse
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+from starlette.status import HTTP_422_UNPROCESSABLE_CONTENT
 
 
 async def http422_error_handler(
@@ -19,5 +19,5 @@ async def http422_error_handler(
     """
     return JSONResponse(
         {"errors": json.loads(exc.json())},
-        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+        status_code=HTTP_422_UNPROCESSABLE_CONTENT,
     )
