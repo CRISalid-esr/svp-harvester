c8db9fa19087a56913c3d34743287bf739a7ac8a
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2026-06-08 03:00:56 +0200

    Add docstring to MessageMode

--- app/models/message_mode.py
+++ app/models/message_mode.py
@@ -2,5 +2,7 @@ from enum import StrEnum
 
 
 class MessageMode(StrEnum):
+    """Routing mode segment appended to every AMQP routing key (5th segment)."""
+
     BATCH = "batch"
     INTERACTIVE = "interactive"
